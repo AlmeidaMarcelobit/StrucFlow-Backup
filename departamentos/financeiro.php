e68118d04800abd36b830ec16e039a838c96475d
Marcelo.Byte <124610030+yKrypt0n@users.noreply.github.com>
2024-06-12 17:18:56 -0300

    🏗️ 3.2.0 - Estrutura de estoque e novos registros
    - 📦 Criação da página `estoque.php`.
    - 🎨 Adicionado `style_estoque.css` para estilização da nova página.
    - 🧑‍💼 Novos registros de colaboradores: Marco Bizarri e Murillo Zanine.
    - 🖼️ Adição de ícone: `laptop.png` (pasta `icones/`).
    - 💼 Criação da estrutura inicial para `financeiro.php`.
    
    - 🧭 Atualização do menu (`header.php`) para incluir novas páginas.
    - 🏠 Atualização na página `home.php`.
    
    - 📁 Organização de imagens e criação da pasta `icones/`.

---
+++ departamentos/financeiro.php
@@ -0,0 +1,236 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet" />
+    <title>Financeiro</title>
+</head>
+<body>
+<?php
+include 'header.php';
+?>
+<main>
+    <header>
+        <h1>COLABORADORES ATIVOS</h1>
+    </header>
+    <article>
+        <h2>DADOS DO COLABORADOR</h2>
+        <section class="colaborador-info">
+            <div class="colaborador-img"> <img src="../foto_colaboradores/FABRICIO FREITAS.jpeg" alt="Foto de Francisco Fabricio De Lima Freitas" class="foto" /> </div>
+            <div class="colaborador-details">
+                <p><strong>Colaborador: </strong>Francisco Fabricio De Lima Freitas</p>
+                <p><strong>Email: </strong>fabricio.freitas@amorsaude.com</p>
+                <p><strong>Cargo: </strong>Gerente Financeiro</p>
+                <p><strong>Departamento: </strong>Financeiro</p>
+                <p><strong>CPF: </strong>582.444.093-04</p>
+                <p><strong>Data da Entrada: </strong>06.maio.2024</p>
+                <p><strong>Data da Saida: </strong>31.maio.2024</p>
+                <p><strong>Centro de Custo: </strong>13000</p>
+                <p><strong>Subtituição: </strong>Roger Almeida</p>
+                <p><strong>Termo de Entrega do Equipamento: </strong><a href="https://drive.google.com/file/d/17ozw0_TG7lOQbbrdgo8frO5b4ef4PTEC/view?usp=sharing" target="_blank">Termo 01</a><a href="https://drive.google.com/file/d/1-MWQ-KQGaTNB5aYQEspkwerq9isaMsrm/view?usp=sharing" target="_blank">Termo 02</a></p>
+            </div>
+        </section>
+        <h2>DADOS DOS EQUIPAMENTOS</h2>
+        <section class="equipamentos-info">
+            <div class="equipamento">
+                <p><strong>Tipo: </strong>Teclado e Mouse</p>
+                <p><strong>Patrimônio: </strong>1525</p>
+                <p class="serial"><strong>S/N: </strong>2232ce1d0218</p>
+                <p><strong>Marca/Modelo: </strong>Logitech MK270</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo: </strong>Celular</p>
+                <p><strong>Patrimônio: </strong>843</p>
+                <p class="serial"><strong>S/N: </strong>UI5HVE5P7T</p>
+                <p><strong>Marca/Modelo: </strong>Moto E22</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo: </strong>Headset</p>
+                <p><strong>Patrimônio: </strong>600</p>
+                <p class="serial"><strong>S/N: </strong>2654439793</p>
+                <p><strong>Marca/Modelo: </strong>Jabra HSC016</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo: </strong>Notebook Win-10 Ryzen5-5500u</p>
+                <p><strong>Patrimônio: </strong>189</p>
+                <p class="serial"><strong>S/N: </strong>PE09FTTP</p>
+                <p><strong>Marca/Modelo: </strong>Ideapad3 15AL6</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo: </strong>Monitor</p>
+                <p><strong>Patrimônio: </strong>1015</p>
+                <p class="serial"><strong>S/N: </strong></p>
+                <p><strong>Marca/Modelo: </strong>Alumínio</p>
+            </div>
+        </section>
+    </article>
+    <article>
+        <h2>DADOS DO COLABORADOR</h2>
+        <section class="colaborador-info">
+            <div class="colaborador-img"> <img src="../foto_colaboradores/Lucas Pedro Pereira.jpeg" alt="Foto de Lucas Pedro Pereira" class="foto" /> </div>
+            <div class="colaborador-details">
+                <p><strong>Colaborador:</strong> Lucas Pedro Pereira</p>
+                <p><strong>Email:</strong> lucas.pereira@amorsaude.com</p>
+                <p><strong>Cargo:</strong> Anaslista Financeiro JR</p>
+                <p><strong>Departamento:</strong> Financeiro</p>
+                <p><strong>CPF:</strong> 461.067.698-26</p>
+                <p><strong>Data da Entrada:</strong> 20.maio.2024</p>
+                <p><strong>Centro de Custo:</strong> 63001</p>
+                <p><strong>Termo de Entrega do Equipamento:</strong><a href="https://drive.google.com/file/d/1P32h-eZIwDYyl6ZieUBolZNPK-KYoV2l/view?usp=sharing" target="_blank">Termo 01</a></p>
+            </div>
+        </section>
+        <h2>DADOS DOS EQUIPAMENTOS</h2>
+        <section class="equipamentos-info">
+            <div class="equipamento">
+                <p><strong>Tipo: </strong> Headset</p>
+                <p><strong>Patrimônio: </strong>1016</p>
+                <p class="serial"><strong>S/N: </strong> 2350AY052028</p>
+                <p><strong>Marca/Modelo: </strong>Logitech H390</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo: </strong>Suporte</p>
+                <p><strong>Patrimônio: </strong>1017</p>
+                <p class="serial"><strong>S/N </strong></p>
+                <p><strong>Marca/Modelo: </strong>Alumínio</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo: </strong>Teclado e Mouse</p>
+                <p><strong>Patrimônio: </strong>1526</p>
+                <p class="serial"><strong>S/N: </strong>2345CE4B4F8</p>
+                <p><strong>Marca/Modelo: </strong>Logitech MK270</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo: </strong>Notebook Win-11 i5-1235U 8GB</p>
+                <p><strong>Patrimônio: </strong>1527</p>
+                <p class="serial"><strong>S/N: </strong>PE0CB4VF</p>
+                <p><strong>Marca/Modelo: </strong>Lenovo V15 G3</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo: </strong>Fonte</p>
+                <p><strong>Patrimônio: </strong>1018</p>
+                <p class="serial"><strong>S/N: </strong>42P69B8</p>
+                <p><strong>Marca/Modelo: </strong>PA-1650-74</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo: </strong>Chip</p>
+                <p><strong>Numero: </strong>16 99749-7166</p>
+            </div>
+        </section>
+        <section class="ticket">
+            <p><strong>ID Ticket:</strong> #31744</p>
+            <p><strong>Equipamento:</strong> Headset, Notebook, Monitor, Suporte, Telcado e Mouse</p>
+        </section>
+    </article>
+
+    <article>
+        <h2>DADOS DO COLABORADOR</h2>
+        <section class="colaborador-info">
+            <div class="colaborador-img"> <img src="../foto_colaboradores/Murillo%20Zanine.jpg" alt="Foto de Murillo Zanine" class="foto" /> </div>
+            <div class="colaborador-details">
+                <p><strong>Colaborador: </strong>Murillo Zanine</p>
+                <p><strong>Email: </strong>murillo.zanine@amorsaude.com</p>
+                <p><strong>Cargo: </strong>Anaslista Financeiro</p>
+                <p><strong>Departamento: </strong>Financeiro</p>
+                <p><strong>CPF: </strong>448.038.248-86</p>
+                <p><strong>Data da Entrada: </strong>4.dezembro.2023</p>
+                <p><strong>Centro de Custo: </strong>13001 - Financeiro</p>
+                <p><strong>Termo de Entrega do Equipamento: </strong><a href="https://drive.google.com/file/d/1d17t7DVPCzyYdCXBrrgaYWiZSTvQHABs/view?usp=sharing" target="_blank">Termo 01</a></p>
+            </div>
+        </section>
+        <h2>DADOS DOS EQUIPAMENTOS</h2>
+        <section class="equipamentos-info">
+            <div class="equipamento">
+                <p><strong>Tipo: </strong> Notebook Win-10 I5-1035G1 8GB</p>
+                <p><strong>Patrimônio: </strong>201</p>
+                <p class="serial"><strong>S/N: </strong> PE09EEM1</p>
+                <p><strong>Marca/Modelo: </strong>Lenovo V15 G2 ITL</p>
+            </div>            <div class="equipamento">
+                <p><strong>Tipo: </strong>Teclado e Mouse</p>
+                <p><strong>Patrimônio: </strong>492</p>
+                <p class="serial"><strong>S/N: </strong> 2229LZXL7GF8</p>
+                <p><strong>Marca/Modelo: </strong>Logitech K270 - M185</p>
+            </div> <div class="equipamento">
+                <p><strong>Tipo: </strong>Fone</p>
+                <p><strong>Patrimônio: </strong>916</p>
+                <p class="serial"><strong>S/N: </strong> 2332ME0207N8</p>
+                <p><strong>Marca/Modelo: </strong>Logitech H390</p>
+            </div>            </div> <div class="equipamento">
+                <p><strong>Tipo: </strong>Suporte</p>
+                <p><strong>Patrimônio: </strong>917</p>
+                <p class="serial"><strong>S/N: </strong> </p>
+                <p><strong>Marca/Modelo: </strong> Alumínio</p>
+            </div>
+        </section>
+        <section class="ticket">
+            <p><Strong>Observação:</Strong> Termo e daodos do monitor faltando</p>
+        </section>
+    </article>
+    <article>
+        <section class="colaborador-info">
+            <div class="colaborador-img">
+                <img src="../foto_colaboradores/Marco%20Bizarri.jpg" alt="Foto Marco Aurélio Bizarri">
+            </div>
+            <div class="colaborador-details">
+                <p><strong>Colaborador: </strong>Marco Aurélio Bizarri</p>
+                <p><strong>Email: </strong>marcos.bizarri@amorsaude.com</p>
+                <p><strong>Cargo: </strong>Analista De Contas A Pagar Pl</p>
+                <p><strong>Departamento: </strong>Financeiro</p>
+                <p><strong>CPF: </strong>412.562.488-78</p>
+                <p><strong>Data da Entrada: </strong>20.junho.2023</p>
+                <p><strong>Centro de Custo: </strong>13001 - Financeiro</p>
+                <p><strong>Termo de Entrega do Equipamento: </strong><a href="https://drive.google.com/file/d/1F1yFWbjJ4CJpfXRvRNkV6GuPBfd6AgsJ/view?usp=sharing">Termo 01</a><a
+                            href="https://drive.google.com/file/d/1ND3ZszXO393AuFwsp5bgRauNXNYhcwtG/view?usp=sharing">Termo 02</a><a
+                            href="https://drive.google.com/file/d/11_MINhHVp_BLYAR-T0gWmafwG9p9fIrB/view?usp=sharing">Termo 03</a></p>
+            </div>
+        </section>
+        <section class="equipamentos-info">
+            <div class="equipamento">
+                <p><strong>Tipo: </strong>Kit Teclado/Mouse</p>
+                <p><strong>Patrimônio: </strong>275</p>
+                <p class="serial"><strong>S/N: </strong>2243CE258479</p>
+                <p class="serial"><strong>S/N: </strong>2241LZX3PAK8</p>
+                <p><strong>Marca/Modelo: </strong>Logitech/MK270</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo: </strong>Suporte Notebook</p>
+                <p><strong>Patrimônio: </strong>815</p>
+                <p class="serial"><strong>S/N: </strong></p>
+                <p><strong>Marca/Modelo: </strong>Alumínio</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo: </strong>Notebook</p>
+                <p><strong>Patrimônio: </strong>166</p>
+                <p class="serial"><strong>S/N: </strong>pe09781x</p>
+                <p><strong>Marca/Modelo: </strong>ideapad3 15alc6</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo: </strong>Headset</p>
+                <p><strong>Patrimônio: </strong>1017</p>
+                <p class="serial"><strong>S/N: </strong>2235ME00Z1J9</p>
+                <p><strong>Marca/Modelo: </strong>LOGITECH / H390</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo: </strong>Smartphone</p>
+                <p><strong>Patrimônio: </strong>1030</p>
+                <p class="serial"><strong>S/N: </strong>r9xw3007pcm</p>
+                <p><strong>IMEI 1: </strong>358302235552506/01</p>
+                <p><strong>IMEI 2: </strong>359796405555202/01</p>
+                <p><strong>Marca/Modelo: </strong>Galaxy A03 Core</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo: </strong>Suporte Monitor</p>
+                <p><strong>Patrimônio: </strong>1452</p>
+                <p class="serial"><strong>S/N: </strong></p>
+                <p><strong>Marca/Modelo: </strong>Multi Suporte</p>
+            </div>
+        </section>
+    </article>
+    <?php
+    include '../footer.php';
+    ?>
+</main>
+</body>
+</html>
