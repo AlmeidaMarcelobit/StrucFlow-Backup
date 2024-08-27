7a2df68c4bd9903d43274105cfc2648941f99ee3
Marcelo.Byte <124610030+yKrypt0n@users.noreply.github.com>
2024-08-27 16:54:50 -0300

    🔧 5.18.0 - Ajustes e Atualizações em Módulos
    - "documentos/update.txt": Atualização no arquivo de controle
    - "dp_administrativo_laboratário.php": Alterações no módulo administrativo de laboratório
    - "dp_amorcirurgias.php": Ajustes na gestão de cirurgias
    - "dp_atendimento_ao_cliente.php": Atualização no atendimento ao cliente
    - "dp_backoffice.php": Alterações no backoffice
    - "dp_canais_digitais_relacionamento.php": Ajustes nos canais digitais de relacionamento
    - "footer.php": Modificações no rodapé do site

--- footer.php
+++ footer.php
@@ -2,3 +2,13 @@
     <p class="reserva">&copy; Reservado AmorSaude</p>
     <p class="version">12.9.1</p>
 </footer>
+
+https://www.youtube.com/
+https://www.youtube.com/watch?v=Zs55IQLMwvQ
+https://www.youtube.com/watch?v=xmDzOJnzfkw
+https://www.youtube.com/@60SegundosRock/videos
+https://www.youtube.com/watch?v=XP3I8ZisWUk
+https://www.youtube.com/watch?v=aTZaorjnEGI
+https://www.youtube.com/watch?v=fnYRSynYQ_s
+https://www.youtube.com/watch?v=GVDJ8O3lPBA
+https://www.youtube.com/watch?v=GA9_QJAhr8Q
