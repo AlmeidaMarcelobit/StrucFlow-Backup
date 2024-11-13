c420cd1e0ffe6bb9f251a6e33bb2debc8d371742
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2024-11-13 11:24:42 -0300

    🎨 6.9.22 - Atualização de links de navegação
    ✨ Adicionado
    - Atualizado o link de "Kits Prontos" para "Estoque" no cabeçalho, garantindo que a navegação esteja consistente com a estrutura atual.

--- departamentos/header.php
+++ departamentos/header.php
@@ -6,6 +6,6 @@
     <a href="financeiro.php">Financeiro</a>
     <a href="CRM.php">CRM</a>
     <a href="maquina_reserva.php">Maquinas Reserva</a>
-    <a href="../kits_prontos.php">Kits Prontos</a>
+    <a href="../estoque.php">Estoque</a>
     <a href="../centro_custo.php">Centro de Custo</a>
 </nav>
