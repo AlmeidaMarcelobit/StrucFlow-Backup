b15a8b38df22b9fb551682243bf95fb5f37d7cc8
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2024-11-13 12:23:37 -0300

    💄 10.1.3
    style(home.php): ajusta a formatação do código e melhora a legibilidade

--- home.php
+++ home.php
@@ -2,43 +2,43 @@
 <html lang="pt-br">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1">
-  <link rel="stylesheet" href="css/all.css">
-  <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet">
-  <title>Home</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="css/all.css">
+    <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet">
+    <title>Home</title>
 </head>
 
 <body>
-  <header>
-    <img src="icones/LOGO_RGB_AMORSAUDE_FINAL_HORIZONTAL.png" alt="LOGO AMORSAUDE">
-  </header>
-  <div class="container">
-    <aside>
-      <nav>
-        <ul>
-          <li><a href="">Home</a></li>
-          <li><a href="">Operação</a></li>
-          <li><a href="">Atendimento ao Cliente</a></li>
-          <li><a href="">Pessoas e Cultura</a></li>
-          <li><a href="">Financeiro</a></li>
-          <li><a href="">CRM</a></li>
-          <li><a href="">Maquina Reserva</a></li>
-          <li><a href="departamentos/estoque.php">Estoque</a></li>
-          <li><a href="">Lista Centro de Custo</a></li>
-        </ul>
-      </nav>
-    </aside>
-    <main>
-      <article>
-        <h1></h1>
-        <p>Este site foi desenvolvido para atender às necessidades de administração de inventário de equipamentos, gestão de dados e termos de responsabilidade. Aqui estão registradas informações detalhadas sobre cada equipamento atribuído a cada funcionário</p>
-      </article>
-    </main>
-  </div>
-  <footer>
-    <p>&copy; Reservado APEX.inc</p>
-  </footer>
+    <header>
+        <img src="icones/LOGO_RGB_AMORSAUDE_FINAL_HORIZONTAL.png" alt="LOGO AMORSAUDE">
+    </header>
+    <div class="container">
+        <aside>
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Operação</a></li>
+                    <li><a href="">Atendimento ao Cliente</a></li>
+                    <li><a href="">Pessoas e Cultura</a></li>
+                    <li><a href="">Financeiro</a></li>
+                    <li><a href="">CRM</a></li>
+                    <li><a href="">Maquina Reserva</a></li>
+                    <li><a href="departamentos/estoque.php">Estoque</a></li>
+                    <li><a href="">Lista Centro de Custo</a></li>
+                </ul>
+            </nav>
+        </aside>
+        <main>
+            <article>
+                <h1></h1>
+                <p>Este site foi desenvolvido para atender às necessidades de administração de inventário de equipamentos, gestão de dados e termos de responsabilidade. Aqui estão registradas informações detalhadas sobre cada equipamento atribuído a cada funcionário</p>
+            </article>
+        </main>
+    </div>
+    <footer>
+        <p>&copy; Reservado APEX.inc</p>
+    </footer>
+<!--16GB 2Rx8 PC4 2400T-SE1--11-->
 </body>
-
 </html>
