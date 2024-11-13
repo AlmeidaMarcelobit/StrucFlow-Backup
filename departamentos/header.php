ede8c70ca2ecbba103763027a90d4097a779196d
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2024-11-12 23:24:07 -0300

    ✨ 4.11.0
    feat(header.php): adiciona arquvio com menu de navegação lateral com links principais

---
+++ departamentos/header.php
@@ -0,0 +1,7 @@
+<nav class="sidebar-menu">
+    <a href="../index.php">Home</a>
+    <a href="atendimento_ao_cliente.php">Atendimento ao Cliente</a>
+    <a href="consultoria.php">Consultoria</a>
+    <a href="pessoas_e_cultura.php">Pessoas e Cultura</a>
+    <a href="../centro_custo.php">Centro de Custo</a>
+</nav>
