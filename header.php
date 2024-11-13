0ae0f6062bf2607d73076cc34620cbe08c3dcabd
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2024-11-12 23:13:46 -0300

    ✨ 4.10.0
    feat(footer.php, header.php): adiciona novos arquivos de cabeçalho e rodapé ao projeto
    
    O cabeçalho inclui um menu de navegação com links para diferentes
    departamentos, enquanto o rodapé contém uma mensagem de direitos
    autorais. Essas adições melhoram a estrutura e a navegação do site.

---
+++ header.php
@@ -0,0 +1,7 @@
+
+<nav class="sidebar-menu">
+    <a href="departamentos/atendimento_ao_cliente.php">Atendimento ao Cliente</a>
+    <a href="departamentos/consultoria.php">Consultoria</a>
+    <a href="departamentos/pessoas_e_cultura.php">Pessoas e Cultura</a>
+    <a href="centro_custo.php">Centro de Custo</a>
+</nav>
