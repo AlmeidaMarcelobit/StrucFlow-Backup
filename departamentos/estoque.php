71c2f593974537f4f9b3105f47d20b16be5df2e5
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2024-11-13 10:51:07 -0300

    🎇  7.0.0
    feat(departamentos): cria novas páginas para os departamentos financeiro e estoque e adiciona links de navegação para financeiro e estoque

---
+++ departamentos/estoque.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/style_estoque.css">
+    <title>Estoque</title>
+</head>
+<body>
+<?php
+include 'header.php';
+?>
+
+<!--80 REIAS LIVIA DIVIDA-->
+<main>
+    <details>
+        <summary>
+            Notebook Reserva.
+        </summary>
+        <article>       
+            <section class="notebooks">
+                <div class="img">
+                <img src="../icones/laptop.png" alt="">
+            </div>
+            <div class="equipamento">
+                    <p><strong>Tipo:</strong>Notebook Win-10 i5-7200 8GB</p>
+                    <p><strong>Patrimonio:</strong>36</p>
+                    <p><strong>Marca\Modelo: </strong>Dell Inspiron 15 3567</p>
+                    <p class="serial"><strong>S/N: </strong>JQD14W2</p>
+            </div>
+        </section>
+        <section class="monitor">
+
+        </section>
+        </article>
+ 
+    </details>
+</main>
+
+</body>
+</html>
