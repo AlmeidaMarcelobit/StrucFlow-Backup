64c158f1441bbf2d6292e8c02765535e44e390dd
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2024-11-13 11:13:46 -0300

    🐛 8.3.1
    fix(header.php): atualiza links de navegação para incluir novas páginas de CRM e máquinas reserva

--- departamentos/header.php
+++ departamentos/header.php
@@ -4,6 +4,8 @@
     <a href="consultoria.php">Consultoria</a>
     <a href="pessoas_e_cultura.php">Pessoas e Cultura</a>
     <a href="financeiro.php">Financeiro</a>
-    <a href="estoque.php">Estoque</a>
+    <a href="CRM.php">CRM</a>
+    <a href="maquina_reserva.php">Maquinas Reserva</a>
+    <a href="../kits_prontos.php">Kits Prontos</a>
     <a href="../centro_custo.php">Centro de Custo</a>
 </nav>
--- header.php
+++ header.php
@@ -1,7 +1,11 @@
 
 <nav class="sidebar-menu">
+    <a href="../Saude-Inventario/home.php">Home</a>
     <a href="departamentos/atendimento_ao_cliente.php">Atendimento ao Cliente</a>
     <a href="departamentos/consultoria.php">Consultoria</a>
     <a href="departamentos/pessoas_e_cultura.php">Pessoas e Cultura</a>
+    <a href="departamentos/financeiro.php">Financeiro</a>
+    <a href="departamentos/maquina_reserva.php">Maquinas Reserva</a>
+    <a href="kits_prontos.php">Kits Prontos</a>
     <a href="centro_custo.php">Centro de Custo</a>
 </nav>
