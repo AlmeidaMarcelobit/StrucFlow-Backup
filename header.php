608323c616849aaa9d6e3864fc096b32978c5f24
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2024-11-13 11:43:52 -0300

    ✨ 6.9.33 - Adição da página Operações.php
    ✨ Adicionado
    - Nova página `Operações.php` para gerenciar as informações relacionadas às operações.

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <nav class="sidebar-menu">
     <a href="../Saude-Inventario/home.php">Home</a>
     <a href="departamentos/atendimento_ao_cliente.php">Atendimento ao Cliente</a>
-    <a href="departamentos/consultoria.php">Consultoria</a>
+    <a href="departamentos/Operações.php.php">Operações</a>
     <a href="departamentos/pessoas_e_cultura.php">Pessoas e Cultura</a>
     <a href="departamentos/financeiro.php">Financeiro</a>
     <a href="departamentos/maquina_reserva.php">Maquinas Reserva</a>
