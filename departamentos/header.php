a2353521b1f988308ea458c7600312b8ad4132f1
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2024-11-13 11:42:26 -0300

    🐛 9.0.4
    fix(header.php): corrige o link da Consultoria para Operações

--- departamentos/header.php
+++ departamentos/header.php
@@ -1,7 +1,7 @@
 <nav class="sidebar-menu">
     <a href="../home.php">Home</a>
     <a href="atendimento_ao_cliente.php">Atendimento ao Cliente</a>
-    <a href="consultoria.php">Consultoria</a>
+    <a href="Operações.php">Oeprações</a>
     <a href="pessoas_e_cultura.php">Pessoas e Cultura</a>
     <a href="financeiro.php">Financeiro</a>
     <a href="CRM.php">CRM</a>
