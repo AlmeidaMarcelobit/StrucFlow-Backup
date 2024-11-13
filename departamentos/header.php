947c6cf5a8aacdacfb3d1fc2b92b27f4583f005c
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2024-11-13 10:37:01 -0300

    🐛 4.11.3
    fix(header.php): corrige o link da página inicial de index.php para home.php

--- departamentos/header.php
+++ departamentos/header.php
@@ -1,5 +1,5 @@
 <nav class="sidebar-menu">
-    <a href="../index.php">Home</a>
+    <a href="../home.php">Home</a>
     <a href="atendimento_ao_cliente.php">Atendimento ao Cliente</a>
     <a href="consultoria.php">Consultoria</a>
     <a href="pessoas_e_cultura.php">Pessoas e Cultura</a>
