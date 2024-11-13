1f8c104e909362cd099eab1eefa78649f6f5d3c1
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2024-11-13 12:03:34 -0300

    🐛 6.9.37 - Ajuste nos links de navegação no header.php
    🔧 Modificado
    - Links de navegação no `header.php` ajustados para refletir os novos arquivos PHP movidos para a raiz:
      - Atualização de links para os departamentos e páginas de operação.

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 
 <nav class="sidebar-menu">
     <a href="../Saude-Inventario/home.php">Home</a>
-    <a href="departamentos/atendimento_ao_cliente.php">Atendimento ao Cliente</a>
+    <a href="dp_atendimento_ao_cliente.php">Atendimento ao Cliente</a>
     <a href="departamentos/Operações.php.php">Operações</a>
-    <a href="departamentos/pessoas_e_cultura.php">Pessoas e Cultura</a>
-    <a href="departamentos/financeiro.php">Financeiro</a>
+    <a href="dp_pessoas_e_cultura.php">Pessoas e Cultura</a>
+    <a href="dp_financeiro.php">Financeiro</a>
     <a href="departamentos/maquina_reserva.php">Maquinas Reserva</a>
-    <a href="estoque.php">Estoque</a>
+    <a href="departamentos/estoque.php">Estoque</a>
     <a href="centro_custo.php">Centro de Custo</a>
 </nav>
