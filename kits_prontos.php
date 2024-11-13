24c619989521ef072a6e03ee894c690518263a90
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2024-11-13 11:17:51 -0300

    ✨ 8.6.0
    feat(kits_prontos.php): implementa página para listar kits ativos e seus detalhes

---
+++ kits_prontos.php
@@ -0,0 +1,270 @@
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet" />
+    <title>Kits Ativos </title>
+</head>
+
+<body>
+<?php
+include 'header.php';
+?>
+<main>
+    <header>
+        <h1>COLABORADORES ATIVOS</h1>
+    </header>
+    <article>
+        <h2>DADOS DO COLABORADOR</h2>
+        <section class="colaborador-info">
+            <div class="colaborador-img"> <img src="foto_colaboradores/Foto_Quem.jpg
+" alt="imagem de quem"> </div>
+            <div class="colaborador-details">
+                <p><strong>Colaborador:</strong></p>
+                <p><strong>Email:</strong></p>
+                <p><strong>Cargo:</strong> Analista</p>
+                <p><strong>Departamento:</strong> Contabilidade</p>
+                <p><strong>CPF:</strong></p>
+                <p><strong>Centro de Custo:</strong> 13003 - Contábil</p>
+                <p><strong>Termo de Entrega de Equipamento:</strong></p>
+            </div>
+        </section>
+
+        <h2>DADOS EQUIPAMENTOS</h2>
+        <section class="equipamentos-info">
+            <div class="equipamento">
+                <p><strong>Tipo:</strong> Notebook Win-11 i5-1235U 8GB</p>
+                <p><strong>Marca/Modelo:</strong> Lenovo V15 G3 IAP</p>
+                <p class="serial"><strong>S/N:</strong> PE9014223078</p>
+                <p><strong>Patrimonio:</strong> 1546</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo:</strong> Suporte</p>
+                <p><strong>Marca/Modelo:</strong> Aluminio</p>
+                <p class="serial"><strong>S/N:</strong></p>
+                <p><strong>Patrimonio:</strong> 1230</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo:</strong> Fone</p>
+                <p><strong>Marca/Modelo:</strong> Logitech H390</p>
+                <p class="serial"><strong>S/N:</strong> 2350ay0512a8</p>
+                <p><strong>Patrimonio:</strong> 1241</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo:</strong> Teclado e Mouse</p>
+                <p><strong>Marca/Modelo:</strong> Logitech Mk270</p>
+                <p class="serial"><strong>S/N:</strong> 2347CE2704D8</p>
+                <p><strong>Patrimonio:</strong> 1545</p>
+            </div>
+        </section>
+
+        <section class="ticket">
+            <p><strong>ID Ticket:</strong> #42722</p>
+            <p><strong>Equipamentos:</strong> Headset, Notebook 8G, Suporte para notebook, Teclado e Mouse, Monitor.</p>
+        </section>
+
+    </article>
+    <article>
+        <h2>DADOS DO COLABORADOR</h2>
+        <section class="colaborador-info">
+            <div class="colaborador-img"> <img src="foto_colaboradores/Foto_Quem.jpg
+" alt="imagem de quem"> </div>
+            <div class="colaborador-details">
+                <p><strong>Colaborador: </strong></p>
+                <p><strong>Email: </strong></p>
+                <p><strong>Cargo: </strong>Assistente</p>
+                <p><strong>Departamento: </strong>Contabilidade</p>
+                <p><strong>CPF: </strong></p>
+                <p><strong>Centro de Custo: </strong>13003 - Contábil</p>
+                <p><strong>Termo de Entrega de Equipamento: </strong></p>
+            </div>
+        </section>
+        <h2>DADOS EQUIPAMENTOS</h2>
+        <section class="equipamentos-info">
+            <div class="equipamento">
+                <p><strong>Tipo:</strong> Suporte</p>
+                <p><strong>Marca/Modelo:</strong> Alumínio</p>
+                <p class="serial"><strong>S/N:</strong></p>
+                <p><strong>Patrimonio:</strong>1232</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo:</strong> Notebook Win-11 i5-1235U 8GB</p>
+                <p><strong>Marca/Modelo:</strong> Lenovo V15 G3 IAP</p>
+                <p class="serial"><strong>S/N:</strong> PE9014223078</p>
+                <p><strong>Patrimonio:</strong> 1542</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo:</strong> Fone</p>
+                <p><strong>Marca/Modelo:</strong> Logitech H390</p>
+                <p class="serial"><strong>S/N:</strong> 2316GG0ASQ8</p>
+                <p><strong>Patrimonio:</strong> 1231</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo:</strong> Teclado e Mouse</p>
+                <p><strong>Marca/Modelo:</strong> Logitech MK270</p>
+                <p class="serial"><strong>S/N:</strong> 2347CE2704e8</p>
+                <p><strong>Patrimonio:</strong> 1547</p>
+            </div>
+        </section>
+        <section class="ticket">
+            <p><strong>ID Ticket:</strong>#42745</p>
+            <p><strong>Equipamento:</strong>Headset, Mouse, Notebook 8G, Suporte para notebook, Teclado, Monitor</p>
+        </section>
+    </article>
+    <article>
+        <h2>DADOS DO COLABORADOR</h2>
+        <section class="colaborador-info">
+            <div class="colaborador-img"> <img src="foto_colaboradores/Foto_Quem.jpg
+" alt="imagem de quem"> </div>
+            <div class="colaborador-details">
+                <p><strong>Colaborador: </strong></p>
+                <p><strong>Email: </strong></p>
+                <p><strong>Cargo: </strong>Assistente</p>
+                <p><strong>Departamento: </strong>Canais Digitais e Relacionamento</p>
+                <p><strong>CPF: </strong></p>
+                <p><strong>Centro de Custo: </strong>22005 - Operação Canais Digitais</p>
+                <p><strong>Termo de Entrega de Equipamento: </strong></p>
+            </div>
+        </section>
+        <h2>DADOS DOS EQUIPAMENTOS</h2>
+        <section class="equipamentos-info">
+            <div class="equipamento">
+                <p><strong>Tipo:</strong>Notebook Win-11 i51235U 8GB</p>
+                <p><strong>Patrimônio:</strong> 1540</p>
+                <p class="serial"><strong>S/N:</strong>peoc3lr2</p>
+                <p><strong>Marca/Modelo:</strong>Lenovo V15 G3 IAP</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo: </strong>Teclado e Mouse</p>
+                <p><strong>Patrimônio: </strong>1537</p>
+                <p lass="serial">S/N: <strong></strong>2347ce268048</p>
+                <p><strong>Marca/Modelo: </strong>Logitech MK270</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo: </strong>Suporte</p>
+                <p><strong>Patrimônio: </strong>1239</p>
+                <p><strong>S/N: </strong></p>
+                <p><strong>Marca/Modelo: </strong>Alumínioo</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo: </strong>Fone</p>
+                <p><strong>Patrimônio: </strong>1240</p>
+                <p><strong>S/N: </strong>2316gg00asm8</p>
+                <p><strong>Marca/Modelo: </strong>Logitech H390</p>
+            </div>
+
+        </section>
+        <section class="ticket">
+            <p><strong>ID Ticket:</strong>#39154</p>
+            <p><strong>Equipamento:</strong>Monitor, Headset, Mouse, Notebook 8G, Suporte para notebook, Teclado</p>
+        </section>
+    </article>
+    <article>
+        <h2>DADOS DO COLABORADOR</h2>
+        <section class="colaborador-info">
+            <div class="colaborador-img"> <img src="foto_colaboradores/Foto_Quem.jpg
+" alt="imagem de quem"> </div>
+            <div class="colaborador-details">
+                <p><strong>Colaborador: </strong></p>
+                <p><strong>Email: </strong></p>
+                <p><strong>Cargo: </strong>Product Owner</p>
+                <p><strong>Departamento: </strong>Canais Digitais e Relacionamento</p>
+                <p><strong>CPF: </strong></p>
+                <p><strong>Centro de Custo: </strong>14003 - CRM</p>
+                <p><strong>Termo de Entrega de Equipamento: </strong></p>
+            </div>
+        </section>
+        <h2>DADOS DOS EQUIPAMENTOS</h2>
+        <section class="equipamentos-info">
+            <div class="equipamento">
+                <p><strong>Tipo:</strong>Fone</p>
+                <p><strong>Patrimônio:</strong>1237</p>
+                <p class="serial"><strong>S/N:</strong>233me034q78</p>
+                <p><strong>Marca/Modelo:</strong>Logitech H390</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo: </strong>Teclado e Mouse</p>
+                <p><strong>Patrimônio: </strong>1536</p>
+                <p lass="serial">S/N: <strong></strong>2347ce270468</p>
+                <p><strong>Marca/Modelo: </strong>Logitech MK270</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo: </strong>Suporte</p>
+                <p><strong>Patrimônio: </strong>1238</p>
+                <p><strong>S/N: </strong></p>
+                <p><strong>Marca/Modelo: </strong>Aluminio</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo: </strong>Notebook</p>
+                <p><strong>Patrimônio: </strong>1541</p>
+                <p><strong>S/N: </strong>PE9014223078</p>
+                <p><strong>Marca/Modelo: </strong>Lenovo V15 G3 AIP</p>
+            </div>
+        </section>
+        <section class="ticket">
+            <p><strong>ID Ticket:</strong>#39154</p>
+            <p><strong>Equipamento:</strong>Headset, Mouse, Notebook 8G, Suporte para notebook, Teclado</p>
+        </section>
+    </article>
+    <article>
+        <h2>DADOS DO COLABORADOR</h2>
+        <section class="colaborador-info">
+            <div class="colaborador-img"> <img src="foto_colaboradores/Foto_Quem.jpg
+" alt="imagem de quem"> </div>
+            <div class="colaborador-details">
+                <p><strong>ID Caixa</strong>#34827</p>
+                <p><strong>Colaborador:</strong></p>
+                <p><strong>Email:</strong></p>
+                <p><strong>Cargo:</strong> Contador</p>
+                <p><strong>Departamento:</strong> Contabilidade</p>
+                <p><strong>CPF:</strong></p>
+                <p><strong>Centro de Custo:</strong> 13003 - Contábil</p>
+                <p><strong>Termo de Entrega de Equipamento:</strong></p>
+            </div>
+        </section>
+
+        <h2>DADOS EQUIPAMENTOS</h2>
+        <section class="equipamentos-info">
+            <div class="equipamento">
+                <p><strong>Tipo:</strong> Notebook Win-11 i5-1235U 8GB</p>
+                <p><strong>Marca/Modelo:</strong> Lenovo V15 G3 IAP</p>
+                <p class="serial"><strong>S/N:</strong> PE0C4SLQ</p>
+                <p><strong>Patrimonio:</strong> 1544</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo:</strong> Fone</p>
+                <p><strong>Marca/Modelo:</strong> Logitech H390</p>
+                <p class="serial"><strong>S/N:</strong> 2316GG00ASR8</p>
+                <p><strong>Patrimonio:</strong> 1242</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo:</strong> Suporte</p>
+                <p><strong>Marca/Modelo:</strong> Aluminio</p>
+                <p class="serial"><strong>S/N:</strong></p>
+                <p><strong>Patrimonio:</strong> 1243</p>
+            </div>
+            <div class="equipamento">
+                <p><strong>Tipo:</strong> Teclado e Mouse</p>
+                <p><strong>Marca/Modelo:</strong> Logitech MK270</p>
+                <p class="serial"><strong>S/N:</strong> 2347CE268008</p>
+                <p><strong>Patrimonio:</strong> 1543</p>
+            </div>
+        </section>
+
+        <section class="ticket">
+            <p><strong>ID Ticket:</strong> #42705</p>
+            <p><strong>Equipamentos:</strong> Headset, Notebook 8G, Suporte para notebook, Teclado e Mouse, Monitor.</p>
+        </section>
+    </article>
+</main>
+<?php
+include 'footer.php';
+?>
+</body>
+<!--34827-->USADO
+<!--59103-->USADO
+<!--47658-->
+<!--93204-->
+<!--61739-->
+
+</html>
