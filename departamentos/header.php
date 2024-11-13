43f5ce8c8027d835bbbf0e012fe5f4571a203164
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2024-11-13 10:50:01 -0300

    🐛 5.0.1
    fix(header.php): atualiza o cabeçalho para incluir novos links de navegação

--- departamentos/header.php
+++ departamentos/header.php
@@ -3,5 +3,7 @@
     <a href="atendimento_ao_cliente.php">Atendimento ao Cliente</a>
     <a href="consultoria.php">Consultoria</a>
     <a href="pessoas_e_cultura.php">Pessoas e Cultura</a>
+    <a href="financeiro.php">Financeiro</a>
+    <a href="estoque.php">Estoque</a>
     <a href="../centro_custo.php">Centro de Custo</a>
 </nav>
