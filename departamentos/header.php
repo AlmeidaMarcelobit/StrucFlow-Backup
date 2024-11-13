4c081ef3528c77b58e85f28f701d2f20730efc0f
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2024-11-13 12:23:50 -0300

    🐛 10.1.4
    fix(header.php): corrige os links das páginas no menu lateral

--- departamentos/header.php
+++ departamentos/header.php
@@ -1,11 +1,11 @@
 <nav class="sidebar-menu">
     <a href="../home.php">Home</a>
-    <a href="atendimento_ao_cliente.php">Atendimento ao Cliente</a>
-    <a href="operações.php">Oeprações</a>
-    <a href="pessoas_e_cultura.php">Pessoas e Cultura</a>
-    <a href="financeiro.php">Financeiro</a>
-    <a href="CRM.php">CRM</a>
+    <a href="../dp_atendimento_ao_cliente.php">Atendimento ao Cliente</a>
+    <a href="../dp_operações.php">Oeprações</a>
+    <a href="../dp_pessoas_e_cultura.php">Pessoas e Cultura</a>
+    <a href="../dp_financeiro.php">Financeiro</a>
+    <a href="../dp_CRM.php">CRM</a>
     <a href="maquina_reserva.php">Maquinas Reserva</a>
-    <a href="../estoque.php">Estoque</a>
+    <a href="estoque.php">Estoque</a>
     <a href="../centro_custo.php">Centro de Custo</a>
 </nav>
