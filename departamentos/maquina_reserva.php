a52a662398d181720de05864d0f0b403e5011cc4
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2024-11-13 11:17:33 -0300

    🎨 6.9.19 - Adição de novas páginas para gerenciamento de colaboradores e equipamentos
    ✨ Adicionado
    - Novas páginas para gerenciamento de colaboradores e equipamentos:
      - `departamentos/CRM.php` - Para gestão de informações do departamento de CRM.
      - `departamentos/Tech.php` - Para o gerenciamento de tecnologia e equipes relacionadas.
      - `departamentos/maquina_reserva.php` - Para administração de máquinas reserva e seus detalhes.

---
+++ departamentos/maquina_reserva.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/style_m_reserva.css">
+    <title>Maquinas Reserva</title>
+</head>
+
+<body>
+<?php
+include 'header.php';
+?>
+<main>
+    <article class="in-stock">
+        <section class="notebooks-info">
+            <div class="img">
+                <img src="../icones/laptop.png" alt="icone laptop">
+            </div>
+            <div class="equipamento-details">
+                <p><strong>Nome: </strong> Reserva-01</p>
+                <p><strong>Tipo: </strong>Notebook Win-10 i5-7200 8GB</p>
+                <p><strong>Patrimonio: </strong>36</p>
+                <p><strong>Marca\Modelo: </strong>Dell Inspiron 15 3567</p>
+                <p class="serial"><strong>S/N: </strong>JQD14W2</p>
+                <p><strong>Alocado:</strong></p>
+            </div>
+        </section>
+    </article>
+
+    <article>
+        <section class="notebooks-info">
+            <div class="img">
+                <img src="../icones/laptop.png" alt="icone laptop">
+            </div>
+            <div class="equipamento-details">
+                <p><strong>Nome: </strong> Reserva - 03</p>
+                <p><strong>Tipo: </strong>Notebook Win-10 i3-6006U 8GB</p>
+                <p><strong>Patrimonio: </strong>50</p>
+                <p><strong>Marca\Modelo: </strong>Dell Inspiron 15 3567</p>
+                <p class="serial"><strong>S/N: </strong>7QMMDQ2</p>
+                <p><strong>Alocado:</strong></p>
+            </div>
+        </section>
+    </article>
+    <article class="off">
+        <section class="notebooks-info">
+            <div class="img">
+                <img src="../icones/laptop.png" alt="icone laptop">
+            </div>
+            <div class="equipamento-details">
+                <p><strong>Nome: </strong>Reserva- 04</p>
+                <p><strong>Tipo: </strong>Notebook Win-11 i3-8130U 8GB</p>
+                <p><strong>Patrimonio: </strong>11</p>
+                <p><strong>Marca\Modelo: </strong>Dell Inspiron 15 3584</p>
+                <p class="serial"><strong>S/N: </strong>H9RJ263</p>
+                <p><strong>Alocado:</strong>Ana Almeida</p>
+            </div>
+        </section>
+    </article>
+    <article>
+        <section class="notebooks-info">
+            <div class="img">
+                <img src="../icones/laptop.png" alt="icone laptop">
+            </div>
+            <div class="equipamento-details">
+                <p><strong>Nome: </strong>Reserva - 05</p>
+                <p><strong>Tipo: </strong>Notebook Win-10 i3-7020U 8GB</p>
+                <p><strong>Patrimonio: </strong>153</p>
+                <p><strong>Marca\Modelo: </strong>Dell Inspiron 15 3567</p>
+                <p class="serial"><strong>S/N: </strong>DQK44W2</p>
+                <p><strong>Alocada: </strong>Janaina Françoza</p>
+            </div>
+        </section>
+    </article>
+    <article class="in-stock">
+        <section class="notebooks-info">
+            <div class="img">
+                <img src="../icones/laptop.png" alt="icone laptop">
+            </div>
+            <div class="equipamento-details">
+                <p><strong>Nome: </strong>Reserva- 06</p>
+                <p><strong>Tipo: </strong>Notebook Win-7 i3-7020U 4GB</p>
+                <p><strong>Patrimonio: </strong>19</p>
+                <p><strong>Marca\Modelo: </strong>Dell inspiron 3584</p>
+                <p class="serial"><strong>S/N: </strong>6F53R23</p>
+                <p><strong>Alocado:</strong></p>
+            </div>
+        </section>
+    </article>
+    <article class="in-stock">
+        <section class="notebooks-info">
+            <div class="img">
+                <img src="../icones/laptop.png" alt="icone laptop">
+            </div>
+            <div class="equipamento-details">
+                <p><strong>Nome: </strong>Reserva- 07</p>
+                <p><strong>Tipo: </strong>Notebook Win-10 i3-60060U 8GB</p>
+                <p><strong>Patrimonio: </strong>09</p>
+                <p><strong>Marca\Modelo: </strong>Dell Inspiron 15 3567</p>
+                <p class="serial"><strong>S/N: </strong>7SMGDQ2</p>
+                <p><strong>Alocado:</strong></p>
+            </div>
+        </section>
+    </article>
+</main>
+<?php
+include '../footer.php';
+?>
+</body>
+</html>
