1782aef934231af5e2ac1d9001dc36b1f6f52370
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2024-11-14 12:59:49 -0300

    ♻️ 6.15.4 - chore(monitores.php):Renomeado monitores.html para monitores.php

--- monitores.php
+++ monitores.php
@@ -1,70 +1,32 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <link rel="stylesheet" href="../css/principal.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Playwrite+DE+Grund:wght@100..400&display=swap" rel="stylesheet">
-    <meta name="author" content="Marcelo de Araujo Almeida">
-    <title>Monitores</title>
-</head>
-<body>
-<header><img src="../icones/logo_amorsaude.png" alt="LOGO AMORSAUDE"></header>
-<?php include 'header.html';?>
-<main>
-    <article>
-        <p><strong>ID caixa:</strong>01</p>
-        <p><strong>Centro Custo:</strong>12010 - Inteligencia de negocios</p>
-        <p><strong>Marca/Modelo:</strong>LG-22MP410-B</p>
-        <p class="serial"><strong>S/N</strong>303AZUJ2V814</p>
-        <p><strong>Patrimônio:</strong>243</p>
-    </article>
-    <article>
-        <p><strong>ID Caixa:</strong>02</p>
-        <p><strong>Centro Custo:</strong>12010 - Inteligência de Negócios</p>
-        <p><strong>Marca/Modelo:</strong>LG 22MP410-B</p>
-        <p class="serial"><strong>S/N:</strong>303AZTH94393</p>
-        <p><strong>Patrimonio:</strong>403</p>
-    </article>
-    <article>
-        <p><strong>ID Caixa:</strong>03</p>
-        <p><strong>Centro Custo:</strong>12010 - Inteligência de Negócios</p>
-        <p><strong>Marca/Modelo:</strong>LG 22MP410-B</p>
-        <p class="serial"><strong>S/N:</strong>303AZCQ8K059</p>
-        <p><strong>Patrimonio:</strong>401</p>
-    </article>
-    <article>
-        <p><strong>ID Caixa:</strong>04</p>
-        <p><strong>Centro Custo:</strong>12007 - Regional 2</p>
-        <p><strong>Marca/Modelo:</strong>LG 22MP410-B</p>
-        <p class="serial"><strong>S/N:</strong>303AZSP94390</p>
-        <p><strong>Patrimonio:</strong>399</p>
-    </article>
-    <article>
-        <p><strong>ID Caixa:</strong>05</p>
-        <p><strong>Centro Custo:</strong>Sem Centro de Custo</p>
-        <p><strong>Marca/Modelo:</strong>LG 22MP410-B</p>
-        <p class="serial"><strong>S/N:</strong>304azqv6r708</p>
-        <p><strong>Patrimonio:</strong>425</p>
-    </article>
-    <article>
-        <p><strong>ID Caixa:</strong>06</p>
-        <p><strong>Centro Custo:</strong>12006 - Regional 1</p>
-        <p><strong>Marca/Modelo:</strong>LG 22MP410-B</p>
-        <p class="serial"><strong>S/N:</strong>403AZBZ6H853</p>
-        <p><strong>Patrimonio:</strong>1480</p>
-        <p><strong>ID Ticket:</strong>94067</p>
-    </article>
-    <article>
-        <p><strong>ID Caixa:</strong>07</p>
-        <p><strong>Centro Custo:</strong>Sem Centro de Custo</p>
-        <p><strong>Marca/Modelo:</strong>Dell P2018HC</p>
-        <p class="serial"><strong>S/N:</strong>BR-00CPG1-TVB00-9CA-11EU-A08</p>
-        <p><strong>Patrimonio:</strong>002</p>
-    </article>
-</main>
-<?php include 'footer.html' ?>
-</body>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="stylesheet" href="css/all.css" />
+        <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet" />
+        <title>Telemedicina</title>
+    </head>
+    <body>
+        <header>
+            <img src="icones/LOGO_RGB_AMORSAUDE_FINAL_HORIZONTAL.png" alt="LOGO AMORSAUDE" />
+        </header>
+        <div class="container">
+            <?php
+            include 'header.php';
+?>
+            <main>
+                <article>
+                    <p><strong>Dono: </strong></p>
+                    <p><strong>Tipo: </strong>Monitor</p>
+                    <p><strong>Marca/Modelo: </strong> LG22MP410</p>
+                    <p><strong>Patrimônio: </strong> 426</p>
+                </article>
+            </main>
+        </div>
+        <?php
+        include 'footer.php'
+        ?>
+    </body>
 </html>
