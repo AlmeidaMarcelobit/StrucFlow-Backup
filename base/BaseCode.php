12eb282cad08ce96864780a4d06905b35df03984
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2025-01-10 17:09:23 -0300

    ✨ 7.71.0 - (BaseCode.php): atualiza a seção de equipamentos para "Equipamentos Comprometidos" e adiciona detalhes específicos

--- base/BaseCode.php
+++ base/BaseCode.php
@@ -1,26 +1,26 @@
 <html>
 
 <head>
-	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width,initial-scale=1" />
-	<link rel="stylesheet" href="../css/estilo-principal.css">
-	<link rel="stylesheet" href="../css/estilo-code.css">
-	<script src="../static/js/search.js"></script>
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-	<link href="https://fonts.googleapis.com/css2?family=Playwrite+DE+Grund:wght@100..400&display=swap" rel="stylesheet" />
-	<meta name="author" content="Marcelo de Araujo Almeida" />
-	<title>Home</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <link rel="stylesheet" href="../css/estilo-principal.css">
+    <link rel="stylesheet" href="../css/estilo-code.css">
+    <script src="../static/js/search.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Playwrite+DE+Grund:wght@100..400&display=swap" rel="stylesheet" />
+    <meta name="author" content="Marcelo de Araujo Almeida" />
+    <title>Home</title>
 </head>
 
 <body>
-	<header id="cabeca">
-		<img src="../assents/logo/logo.png" alt="LOGO AMORSAUDE">
-		<?php include '../includes/header.html';?>
-	</header>
-	<main>
-		<h1>Codigo padrão colaboradores</h1>
-		<pre>
+    <header id="cabeca">
+        <img src="../assents/logo/logo.png" alt="LOGO AMORSAUDE">
+        <?php include '../includes/header.html';?>
+    </header>
+    <main>
+        <h1>Codigo padrão colaboradores</h1>
+        <pre>
 &lt;article&gt;
 	&lt;section class=&quot;colaborador-info&quot;&gt;
 		&lt;div&gt;
@@ -57,19 +57,19 @@
 		&lt;p&gt;&lt;strong&gt;ID Ticket:&lt;/strong&gt;&lt;/p&gt;
 		&lt;p&gt;&lt;strong&gt;Equipamentos:&lt;/strong&gt;&lt;/p&gt;
 	&lt;/section&gt;
-	&lt;h2&gt;Equipamento danificados&lt;/h2&gt;
-	&lt;section class=&quot;equipamentos-info&quot;&gt;
-		&lt;div class=&quot;equipamento-danificados&quot;&gt;
-			&lt;p&gt;&lt;strong&gt;Tipo:&lt;/strong&gt;&lt;/p&gt;
-			&lt;p&gt;&lt;strong&gt;Marca/Modelo:&lt;/strong&gt;&lt;/p&gt;
-			&lt;p class=&quot;serial&quot;&gt;&lt;strong&gt;S/N:&lt;/strong&gt;&lt;/p&gt;
-			&lt;p&gt;&lt;strong&gt;Patrim&ocirc;nio:&lt;/strong&gt;&lt;/p&gt;
-		&lt;/div&gt;
+        &lt;h2&gt;Equipamentos Comprometidos&lt;/h2&gt;
+                &lt;section class=&quot;equipamentos-info&quot;&gt;
+                    &lt;div class=&quot;equipamento-comprometidos&quot;&gt;
+                        &lt;p&gt;&lt;strong&gt;Tipo:&lt;/strong&gt;Fone&lt;/p&gt;
+                        &lt;p&gt;&lt;strong&gt;Marca/Modelo:&lt;/strong&gt;Jabra HSC016&lt;/p&gt;
+                        &lt;p&gt;&lt;strong&gt;S/N:&lt;/strong&gt;265441762&lt;/p&gt;
+                        &lt;p&gt;&lt;strong&gt;Patrim&ocirc;nio:&lt;/strong&gt;598&lt;/p&gt;
+                    &lt;/div&gt;
 	&lt;/section&gt;
 &lt;/article&gt;
 		</pre>
-	</main>
-	<?php include '../includes/footer.html'; ?>
+    </main>
+    <?php include '../includes/footer.html'; ?>
 </body>
 
 </html>
