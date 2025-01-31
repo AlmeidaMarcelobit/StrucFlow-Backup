e0493c0f10a772cf0d6a1dab10edf5bd860876db
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2025-01-31 15:56:03 -0300

    🎁 17.20.7
    Adiciona release versão 2.0.0

---
+++ V2.0.0/includes/header.php
@@ -0,0 +1,18 @@
+<header>
+        <nav>
+            <a href="../pages/colaboradores.php">Colaboradores</a>
+            <div class="menu-dropdown">
+                <a href="#">Estoques</a>
+                <div class="dropdown-content">
+                    <a href="#kits">Kits</a>
+                    <a href="../pages/monitor.php">Monitor</a>
+                    <a href="#computadores">Computadores</a>
+                    <a href="#celulares">Celulares</a>
+                    <a href="#fones">Fones</a>
+                    <a href="#danificados">Danificados</a>
+                </div>
+            </div>
+            <a href="#centro-custo">Centro de Custo</a>
+            <a href="#status-sistemas">Status - Sistemas</a>
+        </nav>
+    </header>
---
+++ V2.0.0/pages/monitor.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventário de Monitores</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/styles-colaboradores.css">
+</head>
+
+<body>
+    <?php include '../includes/header.php'?>
+
+<h1 style="text-align:center; margin-top: 10px">💻 Inventário de Monitores</h1>
+    <div class="container">
+        <!-- Cartões de Monitores -->
+        <div class="card">
+            <h3>💻 ID Caixa: 01</h3>
+            <p><strong>Centro Custo:</strong> 12010 - Inteligência de Negócios</p>
+            <p><strong>Marca/Modelo:</strong> Dell P2018H</p>
+            <p><strong>S/N:</strong> BR-00CPG1-TVB00-9CA-1VEU-A08</p>
+            <p><strong>Patrimônio:</strong> 043</p>
+        </div>
+
+        <div class="card">
+            <h3>💻 ID Caixa: 02</h3>
+            <p><strong>Centro Custo:</strong> Sem Centro Custo</p>
+            <p><strong>Marca/Modelo:</strong> Dell P2018H</p>
+            <p><strong>S/N:</strong> BR-00CPG1-TVB00-9CA-1L9U</p>
+            <p><strong>Patrimônio:</strong> 143</p>
+        </div>
+
+        <div class="card">
+            <h3>💻 ID Caixa: 03</h3>
+            <p><strong>Centro Custo:</strong> 12010 - Inteligência de Negócios</p>
+            <p><strong>Marca/Modelo:</strong> LG 22MP410-B</p>
+            <p><strong>S/N:</strong> 303AZCQ8K059</p>
+            <p><strong>Patrimônio:</strong> 401</p>
+        </div>
+
+        <div class="card">
+            <h3>💻 ID Caixa: 04</h3>
+            <p><strong>Centro Custo:</strong> 12007 - Regional 2</p>
+            <p><strong>Marca/Modelo:</strong> LG 22MP410-B</p>
+            <p><strong>S/N:</strong> 303AZSP94390</p>
+            <p><strong>Patrimônio:</strong> 399</p>
+        </div>
+
+        <div class="card">
+            <h3>💻 ID Caixa: 05</h3>
+            <p><strong>Centro Custo:</strong> 12005 - Backoffice</p>
+            <p><strong>Marca/Modelo:</strong> LG22P410-B</p>
+            <p><strong>S/N:</strong> 303AZZX8K061</p>
+            <p><strong>Patrimônio:</strong> 395</p>
+        </div>
+
+        <div class="card">
+            <h3>💻 ID Caixa: 06</h3>
+            <p><strong>Centro Custo:</strong> 12006 - Regional 1</p>
+            <p><strong>Marca/Modelo:</strong> LG 22MP410-B</p>
+            <p><strong>S/N:</strong> 403AZBZ6H853</p>
+            <p><strong>Patrimônio:</strong> 1480</p>
+        </div>
+
+        <div class="card">
+            <h3>💻 ID Caixa: 07</h3>
+            <p><strong>Centro Custo:</strong> 12006 - Regional 1</p>
+            <p><strong>Marca/Modelo:</strong> LG 22MP410-B</p>
+            <p><strong>S/N:</strong> 112AZB7C018</p>
+            <p><strong>Patrimônio:</strong> 060</p>
+        </div>
+
+        <div class="card">
+            <h3>💻 ID Caixa: 08</h3>
+            <p><strong>Centro Custo:</strong> 12005 - Backoffice</p>
+            <p><strong>Marca/Modelo:</strong> LG 22MP410-B</p>
+            <p><strong>S/N:</strong> 303AZCQ93931</p>
+            <p><strong>Patrimônio:</strong> 394</p>
+        </div>
+
+        <div class="card">
+            <h3>💻 ID Caixa: 09</h3>
+            <p><strong>Centro Custo:</strong> 12006 - Regional 1</p>
+            <p><strong>Marca/Modelo:</strong> LG 22MP410-B</p>
+            <p><strong>S/N:</strong> 402AZDB4W274</p>
+            <p><strong>Patrimônio:</strong> 1472</p>
+        </div>
+
+        <div class="card">
+            <h3>💻 ID Caixa: 10</h3>
+            <p><strong>Centro Custo:</strong> Sem Custo</p>
+            <p><strong>Marca/Modelo:</strong> LG 22MP410-B</p>
+            <p><strong>S/N:</strong> 401AZXC5Y114</p>
+            <p><strong>Patrimônio:</strong> 1501</p>
+        </div>
+
+        <div class="card">
+            <h3>💻 ID Caixa: 11</h3>
+            <p><strong>Centro Custo:</strong> 12006 - Regional 1</p>
+            <p><strong>Marca/Modelo:</strong> LG-22MP410-B</p>
+            <p><strong>S/N:</strong> 403AZPUJ240</p>
+            <p><strong>Patrimônio:</strong> 1475</p>
+        </div>
+
+        <div class="card">
+            <h3>💻 ID Caixa: 12</h3>
+            <p><strong>Centro Custo:</strong> Sem Centro Custo</p>
+            <p><strong>Marca/Modelo:</strong> Dell P2018HC</p>
+            <p><strong>S/N:</strong> BR-00CPG1-TVB00-04E-2FIU-A08</p>
+            <p><strong>Patrimônio:</strong> 039</p>
+        </div>
+
+        <div class="card">
+            <h3>💻 ID Caixa: 13</h3>
+            <p><strong>Centro Custo:</strong> Sem Centro Custo</p>
+            <p><strong>Marca/Modelo:</strong> Dell P2018HC</p>
+            <p><strong>S/N:</strong> BR-00CPG1-TVB00-9CA-1V8U-A08</p>
+            <p><strong>Patrimônio:</strong> 139</p>
+        </div>
+
+        <div class="card">
+            <h3>💻 ID Caixa: 14</h3>
+            <p><strong>Centro Custo:</strong> 12007 - Regional 2</p>
+            <p><strong>Marca/Modelo:</strong> Dell E1920HF</p>
+            <p><strong>S/N:</strong> BR-0V4FTW-TVB00-1A6-8O0B-A03</p>
+            <p><strong>Patrimônio:</strong> 033</p>
+        </div>
+
+        <div class="card">
+            <h3>💻 ID Caixa: 15</h3>
+            <p><strong>Centro Custo:</strong> Sem Centro Custo</p>
+            <p><strong>Marca/Modelo:</strong> Dell E1920HF</p>
+            <p><strong>S/N:</strong> BR-0V4FTW-TVB00-1A6-80QB-A03</p>
+            <p><strong>Patrimônio:</strong> 057</p>
+        </div>
+
+        <div class="card">
+            <h3>💻 ID Caixa: 16</h3>
+            <p><strong>Centro Custo:</strong> 12006 - Regional 1</p>
+            <p><strong>Marca/Modelo:</strong> Dell E1920HF</p>
+            <p><strong>S/N:</strong> BR-OV4FTW-TVB00-1A6-8P0B-A03</p>
+            <p><strong>Patrimônio:</strong> 070</p>
+        </div>
+
+        <div class="card">
+            <h3>💻 ID Caixa: 17</h3>
+            <p><strong>Centro Custo:</strong> Sem Centro Custo</p>
+            <p><strong>Marca/Modelo:</strong> Dell E1920HF</p>
+            <p><strong>S/N:</strong> BR-0V4FTW-TVB00-1A6-8P5B-A03</p>
+            <p><strong>Patrimônio:</strong> 004</p>
+        </div>
+
+        <div class="card">
+            <h3>💻 ID Caixa: 18</h3>
+            <p><strong>Centro Custo:</strong> 13013 - Produto</p>
+            <p><strong>Marca/Modelo:</strong> Dell E1920HF</p>
+            <p><strong>S/N:</strong> BR-0V4FTW-TVB00-1A6-8PRB-A03</p>
+            <p><strong>Patrimônio:</strong> 069</p>
+        </div>
+
+        <div class="card">
+            <h3>💻 ID Caixa: 30</h3>
+            <p><strong>Centro Custo:</strong> Sem Centro Custo</p>
+            <p><strong>Marca/Modelo:</strong> LG22MP410-B</p>
+            <p><strong>S/N:</strong> 303AZQV96500</p>
+            <p><strong>Patrimônio:</strong> Não especificado</p>
+        </div>
+    </div>
+
+    <footer>
+        <p>&copy; 2025 Inventário de Monitores</p>
+    </footer>
+
+</body>
+
+</html>
