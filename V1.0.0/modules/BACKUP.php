b327cfb7e32adcef02a04fd02605fd07646eabf9
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2025-02-04 17:09:32 -0300

    ✨ 9.1.0 - (BACKUP.php): adiciona novo arquivo com informações de colaboradores e equipamentos
    Este commit cria um novo arquivo `BACKUP.php` que contém informações detalhadas sobre colaboradores e os equipamentos que lhes foram atribuídos. A inclusão deste arquivo visa facilitar o gerenciamento e a consulta de dados relacionados aos colaboradores e seus respectivos equipamentos, melhorando a organização e a acessibilidade das informações.

---
+++ V1.0.0/modules/BACKUP.php
@@ -0,0 +1,182 @@
+<article>
+    <section class="colaborador-info">
+        <div><img src="../assents/fotos/foto_colaboradores/Claudia%20Dias.jpg" alt="foto de Claudia Regina Vieria Dias" class="foto"></div>
+        <div class="colaborador-details">
+            <p><strong>Colaborador:</strong>Claudia Regina Vieria Dias</p>
+            <p><strong>E-mail:</strong>claudia.dias@amorsaude.com</p>
+            <p><strong>Cargo:</strong>Assistente Administrativo</p>
+            <p><strong>Departamento:</strong>Telemedicina</p>
+            <p><strong>CPF:</strong>162.236.378-70</p>
+            <p><strong>Data da admissão:</strong>15.05.2023</p>
+            <p><strong>Data da desligamento:</strong></p>
+            <p><strong>Centro de Custo:</strong>22001 - Consulta Assistida</p>
+            <p><strong>Termo de Entrega do Equipamento:</strong></p>
+        </div>
+    </section>
+    <section class="equipamentos-info">
+        <div class="equipamento">
+            <p><strong>Tipo:</strong>Notebook</p>
+            <p><strong>Marca/Modelo:</strong>Dell Inspiron 3501</p>
+            <p class="serial"><strong>S/N:</strong>P90F002</p>
+            <p><strong>Patrimonio:</strong>58</p>
+        </div>
+        <div class="equipamento">
+            <p><strong>Tipo:</strong>Fone</p>
+            <p><strong>Marca/Modelo:</strong>Logitech H390</p>
+            <p class="serial"><strong>S/N:</strong>2235ME00Z1G9</p>
+            <p><strong>Patrimonio:</strong>389</p>
+        </div>
+        <div class="equipamento">
+            <p><strong>Tipo:</strong>Celular</p>
+            <p><strong>Marca/Modelo:</strong>Samsung Galaxy A03</p>
+            <p class="serial"><strong>S/N:</strong>R9XW20CHLOF</p>
+            <p><strong>Patrimonio:</strong></p>
+            <p><strong>IMEI 1:</strong>358302235393566/01</p>
+            <p><strong>IMEI 2:</strong>359796405393562/01</p>
+        </div>
+        <div class="equipamento">
+            <p><strong>Tipo:</strong>Teclado e Mouse</p>
+            <p><strong>Marca/Modelo:</strong>Dell MK 5221</p>
+            <p class="serial"><strong>S/N:</strong>CN-0NCRCC-LO300=34C-K1H2-A01</p>
+            <p><strong>Patrimonio:</strong>430</p>
+        </div>
+        <div class="equipamento">
+            <p><strong>Tipo:</strong>Suporte</p>
+            <p><strong>Marca/Modelo:</strong>Aluminio</p>
+            <p class="serial"><strong>S/N:</strong></p>
+            <p><strong>Patrimonio:</strong>816</p>
+        </div>
+        <div class="equipamento">
+            <p><strong>Tipo:</strong>Monitor</p>
+            <p><strong>Marca/Modelo:</strong>LG 22MP410-P</p>
+            <p class="serial"><strong>S/N:</strong>303AZJT96660</p>
+            <p><strong>Patrimonio:</strong>242</p>
+        </div>
+    </section>
+</article>
+<article>
+    <section class="colaborador-info">
+        <div><img src="../assents/fotos/foto_colaboradores/Daiene%20Priscila.jpg" alt="Foto de Daiene Priscila Alexandre" class="foto"></div>
+        <div class="colaborador-details">
+            <p><strong>Colaborador:</strong>Daiene Priscila Alexandre</p>
+            <p><strong>E-mail:</strong>daiene.priscila@amorsaude.com</p>
+            <p><strong>Cargo:</strong>Analista de Administração Pessoal</p>
+            <p><strong>Departamento:</strong>Departamento Pessoal</p>
+            <p><strong>CPF:</strong>415.939.168-09</p>
+            <p><strong>Data da admissão:</strong></p>
+            <p><strong>Centro de Custo:</strong>13007 - Departamento Pessoal</p>
+        </div>
+    </section>
+    <section class="equipamentos-info">
+        <div class="equipamento">
+            <p><strong>Tipo:</strong>Teclado e Mouse</p>
+            <p><strong>Marca/Modelo:</strong>Logitech MK270</p>
+            <p><strong>S/N:</strong>2243CE258458</p>
+            <p><strong>Patrimônio:</strong>277</p>
+        </div>
+        <div class="equipamento">
+            <p><strong>Tipo:</strong>Notebook</p>
+            <p><strong>Marca/Modelo:</strong>Dell Vostro 15310</p>
+            <p class="serial"><strong>S/N:</strong>CNVMKW3</p>
+            <p><strong>Patrimonio:</strong>78</p>
+        </div>
+        <div class="equipamento">
+            <p><strong>Tipo:</strong>Fone</p>
+            <p><strong>Marca/Modelo:</strong>Jabra HSC016</p>
+            <p class="serial"><strong>S/N:</strong>275243548</p>
+            <p><strong>Patrimonio:</strong>696</p>
+        </div>
+        <div class="equipamento">
+            <p><strong>Tipo:</strong>Celular</p>
+            <p><strong>Marca/Modelo:</strong>Galaxy A03</p>
+            <p class="serial"><strong>S/N:</strong>R9XTA0BDT7D</p>
+            <p><strong>Patrimonio:</strong>695</p>
+            <p><strong>IMEI 1:</strong>352081309398711/01</p>
+            <p><strong>IMEI 2:</strong>357299759398719/01</p>
+        </div>
+    </section>
+</article>
+<article>
+    <section class="colaborador-info">
+        <div><img src="../assents/fotos/foto_colaboradores/Daila%20Santos.jpg" alt="foto de Daila de Souza Silva Santos" class="foto"></div>
+        <div class="colaborador-details">
+            <p><strong>Colaborador:</strong>Daila de Souza Silva Santos</p>
+            <p><strong>E-mail:</strong>daila.santos@amorsaude.com</p>
+            <p><strong>Cargo:</strong>Assistente de Canais Digitais e Relacionamento</p>
+            <p><strong>Departamento:</strong>Canais Digitais e Relacionamento</p>
+            <p><strong>CPF:</strong>438.983.168-23</p>
+            <p><strong>Centro de Custo:</strong>22005 - Operação Canais Digitais</p>
+        </div>
+    </section>
+    <section class="equipamentos-info">
+        <div class="equipamento">
+            <p><strong>Tipo:</strong>Notebook Win-11 i51235U 8GB</p>
+            <p><strong>Patrimônio:</strong>1540</p>
+            <p class="serial"><strong>S/N:</strong>peoc3lr2</p>
+            <p><strong>Marca/Modelo:</strong>Lenovo V15 G3 IAP</p>
+        </div>
+        <div class="equipamento">
+            <p><strong>Tipo:</strong>Teclado e Mouse</p>
+            <p><strong>Patrimônio:</strong>1537</p>
+            <p lass="serial">S/N:<strong></strong>2347ce268048</p>
+            <p><strong>Marca/Modelo:</strong>Logitech MK270</p>
+        </div>
+        <div class="equipamento">
+            <p><strong>Tipo:</strong>Suporte</p>
+            <p><strong>Patrimônio:</strong>1239</p>
+            <p><strong>S/N:</strong></p>
+            <p><strong>Marca/Modelo:</strong>Alumínioo</p>
+        </div>
+        <div class="equipamento">
+            <p><strong>Tipo:</strong>Fone</p>
+            <p><strong>Patrimônio:</strong>1240</p>
+            <p><strong>S/N:</strong>2316gg00asm8</p>
+            <p><strong>Marca/Modelo:</strong>Logitech H390</p>
+        </div>
+    </section>
+    <section class="ticket">
+        <p><strong>ID Ticket:</strong>#39154</p>
+        <p><strong>Equipamento:</strong>Monitor, Headset, Mouse, Notebook 8G, Suporte para notebook, Teclado</p>
+    </section>
+</article>
+<article>
+    <section class="colaborador-info">
+        <div><img src="../assents/fotos/foto_colaboradores/Daniel%20Paula.jpg" alt="foto de Daniel Oliveira Rosa de Paula" class="foto"></div>
+        <div class="colaborador-details">
+            <p><strong>Colaborador:</strong>Daniel Oliveira Rosa de Paula</p>
+            <p><strong>E-mail:</strong>daniel.paula@amorsaude.com</p>
+            <p><strong>Cargo:</strong>Assistent de Cadastro</p>
+            <p><strong>Departamento:</strong>Dental</p>
+            <p><strong>CPF:</strong>441.608.948-10</p>
+            <p><strong>Data da admissão:</strong></p>
+            <p><strong>Data da desligamento:</strong></p>
+            <p><strong>Centro de Custo:</strong>72021 - Cadastro</p>
+        </div>
+    </section>
+    <section class="equipamentos-info">
+        <div class="equipamento">
+            <p><strong>Tipo:</strong>Notebook</p>
+            <p><strong>Marca/Modelo:</strong>Dell Vostro 3520</p>
+            <p class="serial"><strong>S/N:</strong>8R35KZ3</p>
+            <p><strong>Patrimonio:</strong>488</p>
+        </div>
+        <div class="equipamento">
+            <p><strong>Tipo:</strong>Fone</p>
+            <p><strong>Marca/Modelo:</strong>Logitech H390</p>
+            <p class="serial"><strong>S/N:</strong>881-000466</p>
+            <p><strong>Patrimonio:</strong>909</p>
+        </div>
+        <div class="equipamento">
+            <p><strong>Tipo:</strong>Teclado e Mouse</p>
+            <p><strong>Marca/Modelo:</strong>Logitech M185K270</p>
+            <p class="serial"><strong>S/N:</strong>2323LZX5TK68 - 2325CE138058</p>
+            <p><strong>Patrimonio:</strong>497</p>
+        </div>
+        <div class="equipamento">
+            <p><strong>Tipo:</strong>Suporte</p>
+            <p><strong>Marca/Modelo:</strong>Aluminio</p>
+            <p class="serial"><strong>S/N:</strong></p>
+            <p><strong>Patrimonio:</strong>919</p>
+        </div>
+    </section>
+</article>
