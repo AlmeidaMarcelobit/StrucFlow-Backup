070030d8e257e8728cec2543a4a1c33b6c6d09ef
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2025-02-11 09:59:39 -0300

    🐞 6.2.8 - Correção de conflitos e estrutura do HTML no index
    🔄 Refatorado
    🧩 Removidos conflitos de mesclagem no `index.php`
    🧱 Corrigida a estrutura HTML para maior consistência

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
-<!DOCTYPE html>
+<html lang="pt-br">
+
+
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="refresh" content="1; url=departamentos/index.php">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Redirecionando...</title>
 </head>
+
 <body>
     <p>Você será redirecionado em breve. Se não for redirecionado, <a href="outra_pagina.html">clique aqui</a>.</p>
 </body>
+
 </html>
