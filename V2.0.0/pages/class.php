2f5f833cbe0d708221e40ed842ee7a33804dfc41
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2025-02-18 12:52:22 -0300

    🔥 18.18.3
    (class.php): remove arquivo obsoleto de informações importantes sobre equipamentos

--- V2.0.0/pages/class.php
+++
@@ -1,4 +0,0 @@
-<div class="attention-info">
-                <h2>⚠️ Informações Importantes</h2>
-                <p><strong>Alerta:</strong>O colaborador está utilizando os equipamentos, porém ainda não possui o termo de responsabilidade assinado do monitor.</p>
-            </div>
