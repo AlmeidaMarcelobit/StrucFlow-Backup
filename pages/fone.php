5db28856ba725cf17e24391e6e35b66bba675b4b
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2025-03-13 13:13:23 -0300

    🚚 10.21.0 - (fone.html -> fone.php): substitui arquivo HTML por PHP para melhor manutenção e inclusão de cabeçalho dinâmico.

---
+++ pages/fone.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Inventário de Fones</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Marcelo de Araujo Almeida">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/fone.css">
+    <link rel="stylesheet" href="../css/menu-drop.css">
+    <link rel="icon" href="../favicon/favicon-16x16.png">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="../scripts/menudrop.js" defer></script>
+</head>
+
+<body>
+    <?php include '../includes/header.php';  ?>
+    <h2 style="text-align:center; margin-top: 10px">🎧Inventario de Fones</h2>
+    <div class="devices">
+        <div class="device">
+            <h3>📦ID Caixa: 24</h3>
+            <p><strong>Modelo:</strong>Logitech H390</p>
+            <p><strong>S/N:</strong>2309me017vn8</p>
+            <p><strong>Patrimônio:</strong>833</p>
+        </div>
+        <div class="device">
+            <h3>📦ID Caixa: 25</h3>
+            <p><strong>Modelo:</strong>Logitech H390</p>
+            <p><strong>S/N:</strong></p>
+            <p><strong>Patrimônio:</strong>909</p>
+        </div>
+        <div class="device">
+            <h3>📦ID Caixa: 26</h3>
+            <p><strong>Modelo:</strong>Jabra HSC016</p>
+            <p><strong>S/N:</strong>0026441762</p>
+            <p><strong>Patrimônio:</strong>596</p>
+        </div>
+        <div class="device">
+            <h3>📦ID Caixa: 27</h3>
+            <p><strong>Modelo:</strong>Logitech H390</p>
+            <p><strong>S/N:</strong>2125ME0K055</p>
+            <p><strong>Patrimônio:</strong>1125</p>
+        </div>
+        <div class="device">
+            <h3>📦ID Caixa: 28</h3>
+            <p><strong>Modelo:</strong>Logitech H390</p>
+            <p><strong>S/N:</strong>2421AY021RR8</p>
+            <p><strong>Patrimônio:</strong>1098</p>
+        </div>
+        <div class="device">
+            <h3>📦ID Caixa: 29</h3>
+            <p><strong>Modelo:</strong>Logitech H390</p>
+            <p><strong>S/N:</strong>2421AY01UK49</p>
+            <p><strong>Patrimônio:</strong>1123</p>
+        </div>
+        <div class="device">
+            <h3>📦ID Caixa: 30</h3>
+            <p><strong>Modelo:</strong>Jabra HSC016</p>
+            <p><strong>S/N:</strong>00262807419</p>
+            <p><strong>Patrimônio:</strong>535</p>
+        </div>
+        <div class="device">
+            <h3>📦ID Caixa: 31</h3>
+            <p><strong>Modelo:</strong>Jabra HSC016</p>
+            <p><strong>S/N:</strong>273938593</p>
+            <p><strong>Patrimônio:</strong>648</p>
+        </div>
+        <div class="device">
+            <h3>📦ID Caixa: 32</h3>
+            <p><strong>Modelo:</strong>Jabra HSC016</p>
+            <p><strong>S/N:</strong>274048216</p>
+            <p><strong>Patrimônio:</strong>654</p>
+        </div>
+        <div class="device">
+            <h3>📦ID Caixa: 33</h3>
+            <p><strong>Modelo:</strong>Jabra HSC016</p>
+            <p><strong>S/N:</strong>273966069</p>
+            <p><strong>Patrimônio:</strong>650</p>
+        </div>
+        <div class="device">
+            <h3>📦ID Caixa: 34</h3>
+            <p><strong>Modelo:</strong>Logitech H390</p>
+            <p><strong>S/N:</strong>2329gg169w8</p>
+            <p><strong>Patrimônio:</strong>897</p>
+        </div>
+        <div class="device">
+            <h3>📦ID Caixa: 35</h3>
+            <p><strong>Modelo:</strong>Logitech H390</p>
+            <p><strong>S/N:</strong></p>
+            <p><strong>Patrimônio:</strong>766</p>
+        </div>
+    </div>
+    <footer>
+        <p>&copy; 2024 - 2025 SaúdeTracker - Todos os direitos reservados.</p>
+    </footer>
+</body>
+
+</html>
