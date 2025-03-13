70f7c5568737d2892124e40e9a97ee929a4fc958
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2025-03-13 13:17:14 -0300

    ♻️ 20.27.1
    (index.html -> index.php): substitui arquivo HTML por PHP para melhor manutenção e inclusão de cabeçalho dinâmico.

--- index.php
+++ index.php
@@ -2,39 +2,23 @@
 <html lang="pt-br">
 
 <head>
+    <title>Home</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Marcelo de Araujo Almeida">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-16x16.png">
     <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/menu-drop.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="scripts/menudrop.js" defer></script>
-    <title>Home</title>
 </head>
 
 <body>
 
-<!-- Header com Menu -->
-<header>
-    <nav class="nav" aria-label="Main navigation">
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="pages/colaboradores.html">Colaboradores</a></li>
-            <li><a href="#">Centro de Custo</a></li>
-            <li><a href="#">Status - Sistema</a></li>
-            <li class="drop" aria-haspopup="true" aria-expanded="false">
-                <a href="#">Estoque ▾</a>
-                <ul class="dropdown" aria-label="Submenu">
-                    <li><a href="pages/estoque/celular.html">Celular</a></li>
-                    <li><a href="pages/estoque/fone.html">Fones</a></li>
-                    <li><a href="pages/estoque/kits.html">Kits</a></li>
-                    <li><a href="pages/estoque/monitor.html">Monitor</a></li>
-                    <li><a href="pages/estoque/notebook.html">Notebooks</a></li>
-                    <li><a href="pages/estoque/suporte.html">Suporte</a></li>
-                </ul>
-            </li>
-        </ul>
-    </nav>
-</header>
+<?php
+include 'includes/header.php';
+?>
 
 <!-- Conteúdo Principal -->
 <div class="container">
