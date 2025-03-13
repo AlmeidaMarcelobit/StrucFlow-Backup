afbdf85560e0f64677be577bcacd3c74703d8be9
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2025-03-13 12:11:46 -0300

    📦 20.15.2
     (header.php): atualiza a estrutura de navegação para melhorar a acessibilidade  e a usabilidade
    
    Esta mudança foi feita para modernizar o menu de navegação, tornando-o mais acessível e organizado. A nova estrutura utiliza listas não ordenadas e inclui atributos ARIA para melhor suporte a tecnologias assistivas. Além disso, as URLs foram atualizadas para refletir a nova estrutura de diretórios.

--- includes/header.php
+++ includes/header.php
@@ -1,18 +1,21 @@
 <header>
-    <nav>
-        <a href="../pages/colaboradores.php">Colaboradores</a>
-        <div class="menu-dropdown">
-            <a href="#">Estoques</a>
-            <div class="dropdown-content">
-                <a href="../pages/kits.php">Kits</a>
-                <a href="../pages/monitor.php">Monitor</a>
-                <a href="#computadores">Computadores</a>
-                <a href="#celulares">Celulares</a>
-                <a href="#fones">Fones</a>
-                <a href="#danificados">Danificados</a>
-            </div>
-        </div>
-        <a href="#centro-custo">Centro de Custo</a>
-        <a href="#status-sistemas">Status - Sistemas</a>
+    <nav class="nav" aria-label="Main navigation">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="pages/colaboradores.html">Colaboradores</a></li>
+            <li><a href="#">Centro de Custo</a></li>
+            <li><a href="#">Status - Sistema</a></li>
+            <li class="drop" aria-haspopup="true" aria-expanded="false">
+                <a href="#">Estoque ▾</a>
+                <ul class="dropdown" aria-label="Submenu">
+                    <li><a href="pages/estoque/celular.html">Celular</a></li>
+                    <li><a href="pages/estoque/fone.html">Fones</a></li>
+                    <li><a href="pages/estoque/kits.html">Kits</a></li>
+                    <li><a href="pages/estoque/monitor.html">Monitor</a></li>
+                    <li><a href="pages/estoque/notebook.html">Notebooks</a></li>
+                    <li><a href="pages/estoque/suporte.html">Suporte</a></li>
+                </ul>
+            </li>
+        </ul>
     </nav>
 </header>
