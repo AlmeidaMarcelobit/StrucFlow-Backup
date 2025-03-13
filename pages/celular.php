3b0ea19c5623076102c6b431d7819e0dfffe2eae
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2025-03-13 13:10:38 -0300

    🚚 20.18.0
    ♻️ (celular.html -> celular.php): substitui arquivo HTML por PHP para melhor manutenção e inclusão de cabeçalho dinâmico.

---
+++ pages/celular.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Celular</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Marcelo de Araujo Almeida">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/celular.css">
+    <link rel="stylesheet" href="../css/menu-drop.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="../scripts/menudrop.js" defer></script>
+
+</head>
+
+<body>
+    <?php include '../includes/header.php';  ?>
+    <h2 style="text-align:center;margin-top:10px">📱 Inventário de Celular</h2>
+    <div class="container">
+        <div class="card-phone">
+            <h3>Celular 01</h3>
+            <p><strong>Modelo:</strong>Galaxy A03</p>
+            <p><strong>Serie:</strong></p>
+            <p><strong>Patrimonio:</strong>591</p>
+            <p><strong>Status:</strong>Bloqueado</p>
+        </div>
+        <div class="card-phone">
+            <h3>Celular 02</h3>
+            <p><strong>Modelo:</strong>Galaxy A02</p>
+            <p><strong>Serie:</strong></p>
+            <p><strong>Patrimonio:</strong></p>
+            <p><strong>Status:</strong>Bloqueado</p>
+        </div>
+        <div class="card-phone">
+            <h3>Celular 03</h3>
+            <p><strong>Modelo:</strong>Galaxy A03</p>
+            <p><strong>Serie:</strong></p>
+            <p><strong>Patrimonio:</strong>579</p>
+            <p><strong>Status:</strong>Bloqueado</p>
+        </div>
+        <div class="card-phone">
+            <h3>Celular 04</h3>
+            <p><strong>Modelo:</strong>Galaxy A03</p>
+            <p><strong>Serie:</strong></p>
+            <p><strong>Patrimonio:</strong>519</p>
+            <p><strong>Status:</strong>Bloqueado</p>
+        </div>
+        <div class="card-phone">
+            <h3>Celular 05</h3>
+            <p><strong>Modelo:</strong>Galaxy A03</p>
+            <p><strong>Serie:</strong></p>
+            <p><strong>Patrimonio:</strong>538</p>
+            <p><strong>Status:</strong>Bloqueado</p>
+        </div>
+        <div class="card-phone">
+            <h3>Celular 06</h3>
+            <p><strong>Modelo:</strong>Galaxy A03</p>
+            <p><strong>Serie:</strong>R9XTA0EDMWA</p>
+            <p><strong>Patrimonio:</strong>668</p>
+            <p><strong>IMEI 1:</strong>350916877000847</p>
+            <p><strong>IMEI 2:</strong>354654237000845</p>
+        </div>
+        <div class="card-phone">
+            <h3>Celular 07</h3>
+            <p><strong>Modelo:</strong>Galaxy A03</p>
+            <p><strong>Serie:</strong>R9XTA0EFPLL</p>
+            <p><strong>Patrimonio:</strong>667</p>
+            <p><strong>IMEI 1:</strong>350916877023195</p>
+            <p><strong>IMEI 2:</strong>354654237023193</p>
+        </div>
+        <div class="card-phone">
+            <h3>Celular 08</h3>
+            <p><strong>Modelo:</strong>Moto E14</p>
+            <p><strong>Serie:</strong>ZY22K5M9JM</p>
+            <p><strong>Patrimonio:</strong>1171</p>
+            <p><strong>IMEI 1:</strong>354673303308718/07</p>
+            <p><strong>IMEI 2:</strong>354673303308726/07</p>
+        </div>
+        <div class="card-phone">
+            <h3>Celular 09</h3>
+            <p><strong>Modelo:</strong>Galaxy A03</p>
+            <p><strong>Serie:</strong></p>
+            <p><strong>Patrimonio:</strong>510</p>
+            <p><strong>Status:</strong>Bloqueado</p>
+        </div>
+        <div class="card-phone">
+            <h3>Celular 10</h3>
+            <p><strong>Modelo:</strong>Galaxy A03</p>
+            <p><strong>Serie:</strong>R9QT403WRSE</p>
+            <p><strong>Patrimonio:</strong>545</p>
+            <p><strong>IMEI 1:</strong>351989271933664</p>
+            <p><strong>IMEI 2:</strong>353297231933669</p>
+        </div>
+    </div>
+    <footer>
+        <p>&copy; 2024 - 2025 SaúdeTracker - Todos os direitos reservados.</p>
+    </footer>
+</body>
+
+</html>
