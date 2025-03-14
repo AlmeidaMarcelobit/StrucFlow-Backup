e394871a7148e70899455a8e2a2b5bbab88a104a
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2025-03-14 12:09:19 -0300

    ✨ 20.31.0
    (index.php -> includes\index.php): movido arquivo de lugar

--- includes/index.php
+++ includes/index.php
@@ -7,17 +7,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Marcelo de Araujo Almeida">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-16x16.png">
-    <link rel="stylesheet" href="css/index.css">
-    <link rel="stylesheet" href="css/menu-drop.css">
+    <link rel="icon" type="image/png" sizes="32x32" href="../favicon/favicon-16x16.png">
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/menu-drop.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="scripts/menudrop.js" defer></script>
+    <script src="../scripts/menudrop.js" defer></script>
 </head>
 
 <body>
 
 <?php
-include 'includes/header.php';
+include '../includes/header.php';
 ?>
 
 <!-- Conteúdo Principal -->
