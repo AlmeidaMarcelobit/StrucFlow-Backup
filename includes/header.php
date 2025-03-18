d7b306f65aab2f670f7d4b7e59c2a56ab0f5d77a
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2025-03-18 16:49:58 -0300

    🔖 11.0.0

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<header>
+    <nav class="nav" aria-label="Main navigation">
+        <ul>
+            <li><a href="../includes/index.php">Home</a></li>
+            <li><a href="../pages/colaboradores.php">Colaboradores</a></li>
+            <li><a href="#">Centro de Custo</a></li>
+            <li><a href="../pages/monitoramento-park.php">Monitoramento Parque</a></li>
+            <li class="drop" aria-haspopup="true" aria-expanded="false">
+                <a href="#">Estoque ▾</a>
+                <ul class="dropdown" aria-label="Submenu">
+                    <li><a href="../pages/celular.php">Celular</a></li>
+                    <li><a href="../pages/fone.php">Fones</a></li>
+                    <li><a href="../pages/kits.php">Kits</a></li>
+                    <li><a href="../pages/monitor.php">Monitor</a></li>
+                    <li><a href="../pages/notebook.php">Notebooks</a></li>
+                    <li><a href="../pages/suporte.php">Suporte</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</header>
---
+++ pages/suporte.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Suporte</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Marcelo de Araujo Almeida">
+    <link rel="stylesheet" href="../css/suporte.css">
+    <link rel="stylesheet" href="../css/menu-drop.css">
+    <link rel="icon" href="../favicon/favicon-16x16.png">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="../scripts/menudrop.js" defer></script>
+
+</head>
+
+<body>
+    <?php include '../includes/header.php';  ?>
+    <h2>🔩Inventario de Suporte</h2>
+    <div id="container">
+        <div class="card-suporte">
+            <p><strong>Modelo:</strong>Aluminio</p>
+            <p><strong>Subtituição:</strong></p>
+            <p><strong>Patrimonio:</strong>1087</p>
+        </div>
+    </div>
+    <footer>
+        <p>&copy; 2024 - 2025 SaúdeTracker - Todos os direitos reservados.</p>
+    </footer>
+</body>
+
+</html>
