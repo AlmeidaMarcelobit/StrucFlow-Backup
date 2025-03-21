03a6215f9449c0986cc17c032274f03c6cbcd415
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2025-03-21 09:49:43 -0300

    ♻️ 11.6.2 - refactor(code-escape.php): reorganiza seções do código para melhorar a clareza e a manutenção

--- pages/code-escape.php
+++ pages/code-escape.php
@@ -14,11 +14,11 @@
 </head>
 
 <body>
-    <?php include '../includes/header.php';  ?>
-    <div class="container">
-        <div class="box-collaborator">
-            <h2>Codigo Colaboradores</h2>
-            <pre>
+<?php include '../includes/header.php'; ?>
+<div class="container">
+    <div class="box-collaborator">
+        <h2>Codigo Colaboradores</h2>
+        <pre>
                 <code>
                     &lt;div class=&quot;card&quot;&gt;
                         &lt;img src=&quot;&quot; alt=&quot;Foto do Colaborador&quot; class=&quot;profile-pic&quot;&gt;
@@ -28,107 +28,127 @@
                         &lt;p&gt;&lt;strong&gt;E-mail:&lt;/strong&gt;&lt;/p&gt;
                         &lt;p&gt;&lt;strong&gt;CPF:&lt;/strong&gt;&lt;/p&gt;
                         &lt;p&gt;&lt;strong&gt;Centro de Custo:&lt;/strong&gt;&lt;/p&gt;
-                        &lt;p&gt;&lt;span class=&quot;status ativo&quot;&gt;Status:&lt;/span&gt;&lt;/p&gt;
                     &lt;/div&gt;
                 </code>
             </pre>
-        </div>
-
-        <div class="box-icons">
-            <h2>Codigo Icones</h2>
-            <pre>
-               <code>
-                    &lt;h4&gt;⌨️🖱️Teclado e Mouse&lt;/h4&gt;
-                    &lt;h4&gt;💻 Notebook&lt;/h4&gt;
-                    &lt;h4&gt;📑 Termo Respoponsabildiade&lt;/h4&gt;
-                    &lt;h4&gt;🎧 Fone&lt;/h4&gt;
-                    &lt;h4&gt;⚠️ Atenção&lt;/h4&gt;
-                    &lt;h4&gt;📱 Celular&lt;/h4&gt;
-                    &lt;h4&gt;🔌 Fone&lt;/h4&gt;
-                    &lt;h4&gt;🔩 Suporte&lt;/h4&gt;
-                    &lt;h4&gt;🖥️ Monitor&lt;/h4&gt;
-                </code>
-            </pre>
-        </div>
-
-        <div class="box-stock">
-            <h2>Codigo Estoque</h2>
-            <pre>
-                &lt;div class=&quot;card-box caixa-red&quot;&gt;
-                &lt;h2&gt;📦ID Caixa: &lt;/h2&gt;
-                &lt;h3&gt;&lt;/h3&gt;
-            </pre>
-        </div>
-
-        <div class="box-hard">
-            <h2>Codigo Equipamento</h2>
-            <pre>
-                <code>
-                    &lt;div class=&quot;devices&quot;&gt;
-                        &lt;div class=&quot;device&quot;&gt;
-                        &lt;h4&gt;💻 Notebook&lt;&#x2F;h4&gt;
-                            &lt;p&gt;&lt;strong&gt;Patrimônio:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
-                            &lt;p&gt;&lt;strong&gt;Série:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
-                            &lt;p&gt;&lt;strong&gt;Modelo:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
-                            &lt;&#x2F;div&gt;
-                            &lt;div class=&quot;device&quot;&gt;
-                            &lt;h4&gt;⌨️🖱️Teclado e Mouse&lt;&#x2F;h4&gt;
-                            &lt;p&gt;&lt;strong&gt;Patrimônio:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
-                            &lt;p&gt;&lt;strong&gt;Série:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
-                            &lt;p&gt;&lt;strong&gt;Modelo:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
-                            &lt;&#x2F;div&gt;
-                            &lt;div class=&quot;device&quot;&gt;
-                            &lt;h4&gt;🎧 Fone&lt;&#x2F;h4&gt;
-                            &lt;p&gt;&lt;strong&gt;Patrimônio:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
-                            &lt;p&gt;&lt;strong&gt;Série:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
-                            &lt;p&gt;&lt;strong&gt;Modelo:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
-                            &lt;&#x2F;div&gt;
-                            &lt;div class=&quot;device&quot;&gt;
-                            &lt;h4&gt;🔩 Suporte&lt;&#x2F;h4&gt;
-                            &lt;p&gt;&lt;strong&gt;Patrimônio:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
-                            &lt;p&gt;&lt;strong&gt;Série:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
-                            &lt;p&gt;&lt;strong&gt;Modelo:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
-                            &lt;&#x2F;div&gt;
-                            &lt;div class=&quot;device&quot;&gt;
-                            &lt;h4&gt;🖥️ Monitor&lt;&#x2F;h4&gt;
-                            &lt;p&gt;&lt;strong&gt;Patrimônio:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
-                            &lt;p&gt;&lt;strong&gt;Série:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
-                            &lt;p&gt;&lt;strong&gt;Modelo:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
-                            &lt;&#x2F;div&gt;
-                            &lt;div class=&quot;device&quot;&gt;
-                            &lt;h4&gt;📱 Celular&lt;&#x2F;h4&gt;;
-                            &lt;p&gt;&lt;strong&gt;Patrimônio:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
-                            &lt;p&gt;&lt;strong&gt;Série:&lt;&#x2F;strong&gt; &lt;&#x2F;p&gt;
-                            &lt;p&gt;&lt;strong&gt;Modelo:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
-                            &lt;p&gt;&lt;strong&gt;IMEI 1:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
-                            &lt;p&gt;&lt;strong&gt;IMEI 2:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
-                            &lt;&#x2F;div&gt;
-                            &lt;&#x2F;div&gt;
-                            &lt;h2&gt;Equipamentos Comprometidos&lt;&#x2F;h2&gt;
-                            &lt;div class=&quot;device&quot;&gt;
-                            &lt;h4&gt;🎧 Equipamento&lt;&#x2F;h4&gt;
-                            &lt;p&gt;&lt;strong&gt;Patrimônio:&lt;&#x2F;strong&gt; Código&lt;&#x2F;p&gt;
-                            &lt;p&gt;&lt;strong&gt;Série:&lt;&#x2F;strong&gt; Série&lt;&#x2F;p&gt;
-                            &lt;p&gt;&lt;strong&gt;Modelo:&lt;&#x2F;strong&gt; Modelo&lt;&#x2F;p&gt;
-                            &lt;&#x2F;div&gt;
-                            &lt;div class=&quot;attention-info&quot;&gt;
-                            &lt;h2&gt;⚠️ Informações Importantes&lt;&#x2F;h2&gt;
-                            &lt;p&gt;&lt;strong&gt;Alerta:&lt;&#x2F;strong&gt; Informação relevante aqui&lt;&#x2F;p&gt;
-                            &lt;&#x2F;div&gt;
-                            &lt;div class=&quot;devices&quot;&gt;
-                            &lt;div class=&quot;responsibility-link&quot;&gt;
-                            &lt;h4&gt;&lt;a href=&quot;&quot;&gt;📑 Termo de Responsabilidade&lt;&#x2F;a&gt;&lt;&#x2F;h4&gt;
-                            &lt;&#x2F;div&gt;
-                        &lt;&#x2F;div&gt;
-                    &lt;&#x2F;div&gt;
-                </code>
-            </pre>
-        </div>
     </div>
 
-    <footer>
-        <p>&copy; 2024 - 2025 SaúdeTracker - Todos os direitos reservados.</p>
-    </footer>
+
+    <div class="box-hard">
+        <h2>Codigo Status</h2>
+        <pre>
+            <code>
+                &lt;div class=&quot;card&quot;&gt;
+                    &lt;img src=&quot;&quot; alt=&quot;Foto do Colaborador&quot; class=&quot;profile-pic&quot;&gt;
+                    &lt;h3&gt;👤 Nome do Colaborador&lt;/h3&gt;
+                    &lt;p&gt;&lt;strong&gt;Cargo:&lt;/strong&gt;&lt;/p&gt;
+                    &lt;p&gt;&lt;strong&gt;Departamento:&lt;/strong&gt;&lt;/p&gt;
+                    &lt;p&gt;&lt;strong&gt;E-mail:&lt;/strong&gt;&lt;/p&gt;
+                    &lt;p&gt;&lt;strong&gt;CPF:&lt;/strong&gt;&lt;/p&gt;
+                    &lt;p&gt;&lt;strong&gt;Centro de Custo:&lt;/strong&gt;&lt;/p&gt;
+                &lt;/div&gt;
+
+                &lt;p&gt;&lt;span class=&quot;offline&quot;&gt;Status: Offline&lt;/span&gt;&lt;/p&gt;
+                &lt;p&gt;&lt;span class=&quot;online&quot;&gt;Status: Online&lt;/span&gt;&lt;/p&gt;
+            </code>
+         </pre>
+    </div>
+
+    <div class="box-icons">
+        <h2>Codigo Icones</h2>
+        <pre>
+            <code>
+                &lt;h4&gt;⌨️🖱️Teclado e Mouse&lt;/h4&gt;
+                &lt;h4&gt;💻 Notebook&lt;/h4&gt;
+                &lt;h4&gt;📑 Termo Respoponsabildiade&lt;/h4&gt;
+                &lt;h4&gt;🎧 Fone&lt;/h4&gt;
+                &lt;h4&gt;⚠️ Atenção&lt;/h4&gt;
+                &lt;h4&gt;📱 Celular&lt;/h4&gt;
+                &lt;h4&gt;🔌 Fone&lt;/h4&gt;
+                &lt;h4&gt;🔩 Suporte&lt;/h4&gt;
+                &lt;h4&gt;🖥️ Monitor&lt;/h4&gt;
+            </code>
+        </pre>
+    </div>
+
+    <div class="box-stock">
+        <h2>Codigo Estoque</h2>
+        <pre>
+            &lt;div class=&quot;card-box caixa-red&quot;&gt;
+            &lt;h2&gt;📦ID Caixa: &lt;/h2&gt;
+            &lt;h3&gt;&lt;/h3&gt;
+        </pre>
+    </div>
+
+    <div class="box-hard">
+        <h2>Codigo Equipamento</h2>
+        <pre>
+            <code>
+                &lt;div class=&quot;devices&quot;&gt;
+                    &lt;div class=&quot;device&quot;&gt;
+                        &lt;h4&gt;💻 Notebook&lt;&#x2F;h4&gt;
+                        &lt;p&gt;&lt;strong&gt;Patrimônio:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
+                        &lt;p&gt;&lt;strong&gt;Série:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
+                        &lt;p&gt;&lt;strong&gt;Modelo:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
+                    &lt;&#x2F;div&gt;
+                    &lt;div class=&quot;device&quot;&gt;
+                        &lt;h4&gt;⌨️🖱️Teclado e Mouse&lt;&#x2F;h4&gt;
+                        &lt;p&gt;&lt;strong&gt;Patrimônio:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
+                        &lt;p&gt;&lt;strong&gt;Série:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
+                        &lt;p&gt;&lt;strong&gt;Modelo:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
+                    &lt;&#x2F;div&gt;
+                    &lt;div class=&quot;device&quot;&gt;
+                        &lt;h4&gt;🎧 Fone&lt;&#x2F;h4&gt;
+                        &lt;p&gt;&lt;strong&gt;Patrimônio:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
+                        &lt;p&gt;&lt;strong&gt;Série:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
+                        &lt;p&gt;&lt;strong&gt;Modelo:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
+                    &lt;&#x2F;div&gt;
+                    &lt;div class=&quot;device&quot;&gt;
+                        &lt;h4&gt;🔩 Suporte&lt;&#x2F;h4&gt;
+                        &lt;p&gt;&lt;strong&gt;Patrimônio:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
+                        &lt;p&gt;&lt;strong&gt;Série:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
+                        &lt;p&gt;&lt;strong&gt;Modelo:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
+                    &lt;&#x2F;div&gt;
+                    &lt;div class=&quot;device&quot;&gt;
+                        &lt;h4&gt;🖥️ Monitor&lt;&#x2F;h4&gt;
+                        &lt;p&gt;&lt;strong&gt;Patrimônio:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
+                        &lt;p&gt;&lt;strong&gt;Série:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
+                        &lt;p&gt;&lt;strong&gt;Modelo:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
+                    &lt;&#x2F;div&gt;
+                    &lt;div class=&quot;device&quot;&gt;
+                        &lt;h4&gt;📱 Celular&lt;&#x2F;h4&gt;;
+                        &lt;p&gt;&lt;strong&gt;Patrimônio:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
+                        &lt;p&gt;&lt;strong&gt;Série:&lt;&#x2F;strong&gt; &lt;&#x2F;p&gt;
+                        &lt;p&gt;&lt;strong&gt;Modelo:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
+                        &lt;p&gt;&lt;strong&gt;IMEI 1:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
+                        &lt;p&gt;&lt;strong&gt;IMEI 2:&lt;&#x2F;strong&gt;&lt;&#x2F;p&gt;
+                    &lt;&#x2F;div&gt;
+                    &lt;&#x2F;div&gt;
+                        &lt;h2&gt;Equipamentos Comprometidos&lt;&#x2F;h2&gt;
+                        &lt;div class=&quot;device&quot;&gt;
+                        &lt;h4&gt;🎧 Equipamento&lt;&#x2F;h4&gt;
+                        &lt;p&gt;&lt;strong&gt;Patrimônio:&lt;&#x2F;strong&gt; Código&lt;&#x2F;p&gt;
+                        &lt;p&gt;&lt;strong&gt;Série:&lt;&#x2F;strong&gt; Série&lt;&#x2F;p&gt;
+                        &lt;p&gt;&lt;strong&gt;Modelo:&lt;&#x2F;strong&gt; Modelo&lt;&#x2F;p&gt;
+                    &lt;&#x2F;div&gt;
+                    &lt;div class=&quot;attention-info&quot;&gt;
+                        &lt;h2&gt;⚠️ Informações Importantes&lt;&#x2F;h2&gt;
+                        &lt;p&gt;&lt;strong&gt;Alerta:&lt;&#x2F;strong&gt; Informação relevante aqui&lt;&#x2F;p&gt;
+                    &lt;&#x2F;div&gt;
+                    &lt;div class=&quot;devices&quot;&gt;
+                        &lt;div class=&quot;responsibility-link&quot;&gt;
+                            &lt;h4&gt;&lt;a href=&quot;&quot;&gt;📑 Termo de Responsabilidade&lt;&#x2F;a&gt;&lt;&#x2F;h4&gt;
+                        &lt;&#x2F;div&gt;
+                    &lt;&#x2F;div&gt;
+                &lt;&#x2F;div&gt;
+                </code>
+            </pre>
+    </div>
+</div>
+
+<footer>
+    <p>&copy; 2024 - 2025 SaúdeTracker - Todos os direitos reservados.</p>
+</footer>
 </body>
 
 </html>
