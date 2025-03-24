28ca594f910bb3926e0fa74f3a06a95aca609c04
Marcelo Almieda <124610030+yKrypt0n@users.noreply.github.com>
2025-03-24 11:04:26 -0300

    🔗 11.7.2 - (kits.php): corrige a referência do CSS de 'kits.css' para 'colaboradores.css'

--- pages/kits.php
+++ pages/kits.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Marcelo de Araujo Almeida">
-    <link rel="stylesheet" href="../css/kits.css">
+    <link rel="stylesheet" href="../css/colaboradores.css">
     <link rel="stylesheet" href="../css/menu-drop.css">
     <link rel="icon" href="../favicon/favicon-16x16.png">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
