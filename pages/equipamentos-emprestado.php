92bdd484457811ec68e930006c0161ea66600755
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2025-04-14 12:13:31 -0300

    ✨ feat: adiciona página de equipamentos emprestados
    Uma nova página foi criada para exibir informações sobre equipamentos emprestados, incluindo detalhes como modelo, série e patrimônio. Essa adição melhora a funcionalidade do sistema, permitindo que os usuários visualizem facilmente os equipamentos emprestados e suas respectivas informações.

---
+++ pages/equipamentos-emprestado.php
@@ -0,0 +1,26 @@
+
+<html lang="pt-br">
+
+<head>
+    <title>Inventário de Colaboradores</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Marcelo de Araujo Almeida">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="icon" href="../favicon/favicon-16x16.png">
+    <link rel="stylesheet" href="../css/colaboradores.css">
+    <link rel="stylesheet" href="../css/menu-drop.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="../scripts/menudrop.js" defer></script>
+</head>
+
+<body>
+<?php include '../includes/header.php'; ?>
+<h2 style="text-align: center;margin-top: 10px">📋 Emprestado</h2>
+<h1>Teclado</h1>
+<p>Modelo: Logitech MK235 </p>
+<p>Serie: 2207LZX4F248</p>
+<p>Patrimonio: 185 - 578</p>
+<p>Ana Rosa</p>
+</body>
+</html>
