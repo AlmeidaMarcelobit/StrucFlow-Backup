67822f2cdb91c61479ce53cc20899c15b33425d2
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2025-04-16 11:35:10 -0300

    ✨ feat: adiciona novas seções ao menu de colaboradores
    Adiciona as seções B e C ao menu de colaboradores para melhorar a navegação e permitir acesso direto a mais áreas. Corrige a URL da Seção A para refletir a nomenclatura correta, garantindo consistência e evitando possíveis erros de link.

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,9 @@
             <li class="drop" aria-haspopup="true" aria-expanded="false">
                 <a href="#">Colaboradores ▾</a>
                 <ul class="dropdown" aria-label="Submenu">
-                    <li><a href="../colaboradores/Seção%20A.php">Seção A</a></li>
+                    <li><a href="../colaboradores/Secao%20A.php">Seção A</a></li>
+                    <li><a href="../colaboradores/Secao%20B.php">Seção B</a></li>
+                    <li><a href="../colaboradores/Secao%20C.php">Seção C</a></li>
                 </ul>
             </li>
 
