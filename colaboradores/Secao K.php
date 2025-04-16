6dbe2921415b70e16a03785d75d95dfcf8531b7c
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2025-04-16 13:53:59 -0300

    ✨ feat: adiciona nova seção de colaboradores com letra K
    Adiciona uma nova página HTML para listar colaboradores cujos nomes começam com a letra K. A página inclui informações detalhadas sobre cada colaborador, como cargo, departamento, e equipamentos utilizados. Esta adição visa melhorar a organização e acessibilidade das informações dos colaboradores, facilitando a consulta e gestão de dados.

---
+++ colaboradores/Secao K.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Seção H</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Marcelo de Araujo Almeida">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="icon" href="../favicon/favicon-16x16.png">
+    <link rel="stylesheet" href="../css/colaboradores.css">
+    <link rel="stylesheet" href="../css/menu-drop.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="../scripts/menudrop.js" defer></script>
+</head>
+
+<body>
+<?php include '../includes/header.php'; ?>
+<h2 style="text-align: center;margin-top: 10px">📋 Colaboradores Letras K</h2>
+<div class="container">
+
+    <div class="card">
+        <img src="../imagem/Karen%20Ramos.png" alt="Foto do Colaborador" class="profile-pic">
+        <h3>👤 Karen Ramos</h3>
+        <p><strong>Cargo: </strong> Analista De Treinamentos</p>
+        <p><strong>Departamento: </strong> BackOffice</p>
+        <p><strong>E-mail: </strong> karen.ramos@amorsaude.com</p>
+        <p><strong>CPF: </strong> 405.22.938-08</p>
+        <p><strong>Centro de Custo: </strong> 13020 - Treinamentos</p>
+        <p><span class=" online">: </span></p>
+        <div class="devices">
+            <div class="device">
+                <h4>🖥️ Monitor</h4>
+                <p><strong>Patrimônio: </strong> 396</p>
+                <p><strong>Série: </strong> 303AZWS94377</p>
+                <p><strong>Modelo: </strong> LG 22MP410-B</p>
+            </div>
+        </div>
+        <div class="attention-info">
+            <h2>⚠️ Informações Importantes</h2>
+            <p><strong>Alerta: </strong> O colaborador está utilizando os equipamentos listados, porém apenas possui
+                o termo de responsabilidade assinado para o monitor, não tendo os dados e termo para os demais
+                itens.</p>
+        </div>
+        <div class="devices">
+            <div class="responsibility-link">
+                <h4><a href="">📑 Termo de Responsabilidade</a></h4>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <img src="../imagem/Karen%20Loureiro.png" alt="Foto do Colaborador" class="profile-pic">
+        <h3>👤 Karen Loureiro</h3>
+        <p><strong>Cargo: </strong> Consultora de Operações</p>
+        <p><strong>Departamento: </strong> Consultoria</p>
+        <p><strong>CPF: </strong> 387.565.348-33</p>
+        <p><strong>Centro de Custo: </strong>12006 - Regional 1</p>
+        <p><span class="offline">Status: Offline </span></p>
+        <div class="devices">
+            <div class="device">
+                <h4>💻 Notebook</h4>
+                <p><strong>Patrimônio: </strong> 322</p>
+                <p><strong>Série: </strong> 80G8YX3</p>
+                <p><strong>Modelo: </strong> Dell Vostro 15 3510</p>
+            </div>
+            <div class="device">
+                <h4>🎧 Fone</h4>
+                <p><strong>Patrimônio: </strong> 879</p>
+                <p><strong>Série: </strong> 2316ME027FK9</p>
+                <p><strong>Modelo: </strong> Logitech H390</p>
+            </div>
+            <div class="device">
+                <h4>🔩 Suporte</h4>
+                <p><strong>Patrimônio: </strong> 826</p>
+                <p><strong>Série: </strong></p>
+                <p><strong>Modelo: </strong> Alumínio</p>
+            </div>
+            <div class="device">
+                <h4>⌨️🖱️ Teclado e Mouse</h4>
+                <p><strong>Patrimônio: </strong> 325</p>
+                <p><strong>Série: </strong> CN-ONCRCC-LO300-311-KOUH-A01</p>
+                <p><strong>Modelo: </strong> Dell KM5221W</p>
+            </div>
+        </div>
+        <h2>Equipamentos Comprometidos</h2>
+        <div class="device">
+            <h4>🎧 Fone</h4>
+            <p><strong>Patrimônio: </strong> 598</p>
+            <p><strong>Série: </strong> 265441762</p>
+            <p><strong>Modelo: </strong> Jabra HSC016</p>
+        </div>
+        <div class="devices">
+            <div class="responsibility-link">
+                <h4><a href="">📑 Termo de Responsabilidade</a></h4>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <img src="../imagem/Sem%20Foto.png" alt="Foto do Colaborador" class="profile-pic">
+        <h3>👤 Katia Tatiana Tomaz Marcondes</h3>
+        <p><strong>Cargo: </strong> Assistente de Atendimento ao Cliente</p>
+        <p><strong>Departamento: </strong> Atendimento ao Cliente</p>
+        <p><strong>E-mail: </strong>katia.marcondes@amorsaude.com</p>
+        <p><strong>CPF: </strong> 317.842.138-61</p>
+        <p><strong>Centro de Custo: </strong> 12002 - Sac</p>
+        <p><span class=" online">: </span></p>
+        <div class="devices">
+            <div class="device">
+                <h4>🎧 Headset</h4>
+                <p><strong>Patrimônio: </strong> 1020</p>
+                <p><strong>Série: </strong> 23331AY020B49</p>
+                <p><strong>Modelo: </strong> Logitech H390</p>
+            </div>
+            <div class="device">
+                <h4>🔩 Suporte</h4>
+                <p><strong>Patrimônio: </strong> 1019</p>
+                <p><strong>Série: </strong></p>
+                <p><strong>Modelo: </strong> Alumínio</p>
+            </div>
+            <div class="device">
+                <h4>⌨️🖱️ Teclado e Mouse</h4>
+                <p><strong>Patrimônio: </strong> 1529</p>
+                <p><strong>Série: </strong> 2345CE4BA518</p>
+                <p><strong>Modelo: </strong> Logitech MK270</p>
+            </div>
+            <div class="device">
+                <h4>📱 Celular</h4>
+                <p><strong>Patrimônio: </strong> 1228</p>
+                <p><strong>Série: </strong> ZF5244C2KF</p>
+                <p><strong>Modelo: </strong> Moto E22</p>
+                <p><strong>IMEI 1: </strong> 352800824226754</p>
+                <p><strong>IMEI 2: </strong> 352800824226762</p>
+            </div>
+            <div class="device">
+                <h4>🖥️ Monitor</h4>
+                <p><strong>Patrimônio: </strong> 1477</p>
+                <p><strong>Série: </strong> 403AZBZ6J261</p>
+                <p><strong>Modelo: </strong> LG 22MP410-B</p>
+            </div>
+            <div class="device">
+                <h4>💻 Notebook Win-11 i5-1235U 8GB</h4>
+                <p><strong>Patrimônio: </strong> 1528</p>
+                <p><strong>Série: </strong> PEOC4E5</p>
+                <p><strong>Modelo: </strong> Lenovo V15 G3</p>
+            </div>
+        </div>
+        <div class="devices">
+            <div class="responsibility-link">
+                <h4><a href="">📑 Termo de Responsabilidade</a></h4>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <img src="../imagem/K%C3%A9ssya%20Rosolini.png" alt="Foto do Colaborador" class="profile-pic">
+        <h3>👤 Kessya De Lima Rosolini</h3>
+        <p><strong>Cargo: </strong> Coordenador(A) De Pessoas E Cultura</p>
+        <p><strong>Departamento: </strong> Pessoas E Cultura</p>
+        <p><strong>CPF: </strong> 433.684.318-03</p>
+        <p><strong>Centro de Custo: </strong> 13006</p>
+        <p><span class=" online">: </span></p>
+        <div class="devices">
+            <div class="device">
+                <h4>💻 Notebook</h4>
+                <p><strong>Patrimônio: </strong> 405</p>
+                <p><strong>Série: </strong> JRFQXW3</p>
+                <p><strong>Modelo: </strong> Dell Vostro 15 3510</p>
+            </div>
+            <div class="device">
+                <h4>🎧 Fone</h4>
+                <p><strong>Patrimônio: </strong></p>
+                <p><strong>Série: </strong> 595</p>
+                <p><strong>Modelo: </strong> Jabra HSC016</p>
+            </div>
+            <div class="device">
+                <h4>📱 Celular</h4>
+                <p><strong>Patrimônio: </strong> 1028</p>
+                <p><strong>Série: </strong> R9XW20CHL5J</p>
+                <p><strong>Modelo: </strong> Galaxy A03 Core</p>
+                <p><strong>IMEI 1: </strong> 358302235393616/01</p>
+                <p><strong>IMEI 2: </strong> 3597964053393612/01</p>
+            </div>
+        </div>
+        <div class="attention-info">
+            <h2>⚠️ Informações Importantes</h2>
+            <p><strong>Alerta: </strong> O colaborador está utilizando os equipamentos listados, porém não possuímos
+                o termo de responsabilidade do teclado e mouse.</p>
+        </div>
+        <div class="devices">
+            <div class="responsibility-link">
+                <h4><a href="">📑 Termo de Responsabilidade</a></h4>
+            </div>
+        </div>
+    </div>
+</div>
+<footer>
+    <p>&copy; 2024 - 2025 Inventário de Colaboradores - Todos os direitos reservados.</p>
+</footer>
+
+</body>
+</html>
