103d63c9210138ead3f6887c654a886422917f51
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2025-04-16 13:55:46 -0300

    ✨ feat: adiciona nova página para colaboradores da Seção N
    A nova página "Seção N" foi criada para listar colaboradores cujos nomes começam com a letra N. Esta adição melhora a organização e acessibilidade das informações dos colaboradores, permitindo uma navegação mais intuitiva e segmentada. A página inclui detalhes como cargo, departamento, e-mail, CPF, centro de custo, e equipamentos associados a cada colaborador, além de links para termos de responsabilidade.

---
+++ colaboradores/Secao N.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Seção N</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Marcelo de Araujo Almeida">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="icon" href="../favicon/favicon-16x16.png">
+    <link rel="stylesheet" href="../css/colaboradores.css">
+    <link rel="stylesheet" href="../css/menu-drop.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="../scripts/menudrop.js" defer></script>
+</head>
+
+<body>
+<?php include '../includes/header.php'; ?>
+<h2 style="text-align: center;margin-top: 10px">📋 Colaboradores Letras N</h2>
+<div class="container">
+    <div class="card">
+        <img src="../imagem/Sem%20Foto.png" alt="Foto do Colaborador" class="profile-pic">
+        <h3>👤 Natalia Duarte Zimmermann</h3>
+        <p><strong>Cargo: </strong> Coordenadora de Operações</p>
+        <p><strong>Departamento: </strong> Consultorria</p>
+        <p><strong>E-mail: </strong> natalia.zimmermann@amorsaude.com</p>
+        <p><strong>CPF: </strong> 142.180.767-07</p>
+        <p><strong>Centro de Custo: </strong> 12006 - Regional 2</p>
+        <p><span class=" online">:  online</span></p>
+        <div class="devices">
+            <div class="device">
+                <h4>💻 Notebook</h4>
+                <p><strong>Patrimônio: </strong> 166</p>
+                <p><strong>Série: </strong> PE0981X</p>
+                <p><strong>Modelo: </strong> Ideapad3 15alc6</p>
+            </div>
+            <div class="device">
+                <h4>🎧 Fone</h4>
+                <p><strong>Patrimônio: </strong> 692</p>
+                <p><strong>Série: </strong> 27536253</p>
+                <p><strong>Modelo: </strong> Jabra HSC016</p>
+            </div>
+            <div class="device">
+                <h4>⌨️🖱️ Teclado e Mouse</h4>
+                <p><strong>Patrimônio: </strong> 185 - 578</p>
+                <p><strong>Série: </strong> 2207LZX4F248</p>
+                <p><strong>Modelo: </strong> Logitech MK235</p>
+            </div>
+            <div class="device">
+                <h4>📱 Celular</h4>
+                <p><strong>Patrimônio: </strong> 511</p>
+                <p><strong>Série: </strong> R9QT403WMHP</p>
+                <p><strong>Modelo: </strong> Samsung Galaxy A03</p>
+                <p><strong>IMEI 1: </strong> 35198927132252/01</p>
+                <p><strong>IMEI 2: </strong> 3532723193225/01</p>
+            </div>
+        </div>
+        <div class="devices">
+            <div class="responsibility-link">
+                <h4><a href="">📑 Termo de Responsabilidade</a></h4>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <img src="../imagem/Natanel%20Zavanela.png" alt="Foto do Colaborador" class="profile-pic">
+        <h3>👤 Natanael Zavanela</h3>
+        <p><strong>Cargo: </strong> Consultor De Operações</p>
+        <p><strong>Departamento: </strong> Regional 1</p>
+        <p><strong>E-mail: </strong> natanael.zavanela@amorsaude.com</p>
+        <p><strong>CPF: </strong> 330.324458-84</p>
+        <p><strong>Centro de Custo: </strong> 12007 - Regiona 2</p>
+        <p><span class=" online">:  online</span></p>
+        <div class="devices">
+            <div class="device">
+                <h4>🔌 Adaptador de Rede</h4>
+                <p><strong>Patrimônio: </strong> 1216</p>
+                <p><strong>Série: </strong> 223B92011087</p>
+                <p><strong>Modelo: </strong> UE300C</p>
+            </div>
+            <div class="device">
+                <h4>🎧 Fone</h4>
+                <p><strong>Patrimônio: </strong> 889</p>
+                <p><strong>Série: </strong> 2320AY099929</p>
+                <p><strong>Modelo: </strong> Logitech H390</p>
+            </div>
+            <div class="device">
+                <h4>💻 Notebook</h4>
+                <p><strong>Patrimônio: </strong> 168</p>
+                <p><strong>Série: </strong> pe097838</p>
+                <p><strong>Modelo: </strong> Dell Ideapad3 15alc6 Ryzen5-5500u Win-10 8Gb 256GB</p>
+            </div>
+            <div class="device">
+                <h4>⌨️🖱️ Teclado e Mouse</h4>
+                <p><strong>Patrimônio: </strong> 1498</p>
+                <p><strong>Série: </strong> 2232CE1C7128 - 2229LZK3028</p>
+                <p><strong>Modelo: </strong> Logitech MK270 - M170</p>
+            </div>
+            <div class="device">
+                <h4>📱 Celular</h4>
+                <p><strong>Patrimônio: </strong> 739</p>
+                <p><strong>Série: </strong> r9xta0ecssv</p>
+                <p><strong>Modelo: </strong> Samsung A03</p>
+                <p><strong>IMEI 1: </strong> 350916876991574/01</p>
+                <p><strong>IMEI 2: </strong> 354654236991572/01</p>
+            </div>
+        </div>
+        <div class="devices">
+            <div class="responsibility-link">
+                <h4><a href="">📑 Termo de Responsabilidade</a></h4>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <img src="../imagem/Nicholas%20Lopes.png" alt="Foto do Colaborador" class="profile-pic">
+        <h3>👤 Nicholas Esdras Lopes dos Santos</h3>
+        <p><strong>Cargo: </strong> Assistente de BackOffice</p>
+        <p><strong>Departamento: </strong> BackOffice - Treinamentos</p>
+        <p><strong>E-mail: </strong> nicholas.lopes@amorsaude.com</p>
+        <p><strong>CPF: </strong> 462.083.518-80</p>
+        <p><strong>Centro de Custo: </strong> 12005</p>
+        <p><strong>Substituição: </strong> Letícia Godoy da Silva</p>
+        <p><span class=" online">:  online</span></p>
+        <div class="devices">
+            <div class="device">
+                <h4>💻 Notebook</h4>
+                <p><strong>Patrimônio: </strong> 5</p>
+                <p><strong>Série: </strong> 17N0513</p>
+                <p><strong>Modelo: </strong> Dell Inspiron 3501</p>
+            </div>
+            <div class="device">
+                <h4>🎧 Fone</h4>
+                <p><strong>Patrimônio: </strong> 953</p>
+                <p><strong>Série: </strong> 2329AY019B08</p>
+                <p><strong>Modelo: </strong> Logitech H390</p>
+            </div>
+            <div class="device">
+                <h4>🔩 Suporte</h4>
+                <p><strong>Patrimônio: </strong> 962</p>
+                <p><strong>Série: </strong> Não fornecido</p>
+                <p><strong>Modelo: </strong> Alumínio</p>
+            </div>
+            <div class="device">
+                <h4>📱 Celular</h4>
+                <p><strong>Patrimônio: </strong> 716</p>
+                <p><strong>Série: </strong> R9XT202F3JW</p>
+                <p><strong>Modelo: </strong> Samsung Galaxy A03</p>
+                <p><strong>IMEI 1: </strong> 354985980107183/01</p>
+                <p><strong>IMEI 2: </strong> 355106270107184/01</p>
+            </div>
+            <div class="device">
+                <h4>⌨️🖱️ Teclado e Mouse</h4>
+                <p><strong>Patrimônio: </strong> 182 - 571</p>
+                <p><strong>Série: </strong> 2211CE0A2F69</p>
+                <p><strong>Modelo: </strong> MK235 - M170</p>
+            </div>
+        </div>
+        <div class="devices">
+            <div class="responsibility-link">
+                <h4><a href="">📑 Termo de Responsabilidade</a></h4>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <img src="../imagem/Nicolas%20Tassinari.png" alt="Foto do Colaborador" class="profile-pic">
+        <h3>👤 Nicolas Francisco Eloy Tassinari</h3>
+        <p><strong>Cargo: </strong> Analista De Processos</p>
+        <p><strong>Departamento: </strong> BackOffice</p>
+        <p><strong>E-mail: </strong> nicolas.tassinari@amorsaude.com</p>
+        <p><strong>CPF: </strong> 450.326.278-52</p>
+        <p><strong>Centro de Custo: </strong> 12005 - Backoffice</p>
+        <p><span class=" online">:  online</span></p>
+        <div class="devices">
+            <div class="device">
+                <h4>⌨️🖱️ Teclado e Mouse</h4>
+                <p><strong>Patrimônio: </strong> 1458</p>
+                <p><strong>Série: </strong> 2239CE14C8C8</p>
+                <p><strong>Modelo: </strong> Logitech MK270</p>
+            </div>
+            <div class="device">
+                <h4>🖥️ Monitor</h4>
+                <p><strong>Patrimônio: </strong> 395</p>
+                <p><strong>Série: </strong> 303AZZX8K061</p>
+                <p><strong>Modelo: </strong> LG 22MP410-B</p>
+            </div>
+            <div class="device">
+                <h4>💻 Notebook</h4>
+                <p><strong>Patrimônio: </strong> 25</p>
+                <p><strong>Série: </strong> h7lyxp3</p>
+                <p><strong>Modelo: </strong> Dell Vostro 1535P112F</p>
+            </div>
+            <div class="device">
+                <h4>🎧 Fone</h4>
+                <p><strong>Patrimônio: </strong> 780</p>
+                <p><strong>Série: </strong> 2125me0jz328</p>
+                <p><strong>Modelo: </strong> Logitech H390</p>
+            </div>
+            <div class="device">
+                <h4>📱 Celular</h4>
+                <p><strong>Patrimônio: </strong> 579</p>
+                <p><strong>Série: </strong> R9XT700ELMH</p>
+                <p><strong>Modelo: </strong> Galaxy A03</p>
+                <p><strong>IMEI 1: </strong> 354985988574632/01</p>
+                <p><strong>IMEI 2: </strong> 355106278574633/01</p>
+            </div>
+        </div>
+        <div class="attention-info">
+            <h2>⚠️ Informações Importantes</h2>
+            <p><strong>Alerta: </strong> O colaborador está utilizando os equipamentos listados, porém ainda não
+                possui o termo de responsabilidade assinado para os demais itens, exceto pelo kit de teclado, mouse
+                e monitor.</p>
+        </div>
+        <div class="devices">
+            <div class="responsibility-link">
+                <h4><a href="">📑 Termo de Responsabilidade</a></h4>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <img src="../imagem/Nicole%20Souza.png" alt="Foto do Colaborador" class="profile-pic">
+        <h3>👤 Nicole Souza Cimento</h3>
+        <p><strong>Cargo: </strong> Consultor de Operações</p>
+        <p><strong>Departamento: </strong> Consultoria</p>
+        <p><strong>E-mail: </strong> nicole.souza@amorsaude.com</p>
+        <p><strong>CPF: </strong> 464.476.338.19</p>
+        <p><strong>Centro de Custo: </strong> 12007 - Regional 2</p>
+        <p><span class=" online">:  online</span></p>
+        <div class="devices">
+            <div class="device">
+                <h4>⌨️🖱️ Teclado e Mouse</h4>
+                <p><strong>Patrimônio: </strong> 282</p>
+                <p><strong>Série: </strong> 2243CE258429 - 2241LZX3P288</p>
+                <p><strong>Modelo: </strong> Logitech MK270</p>
+            </div>
+            <div class="device">
+                <h4>💻 Notebook</h4>
+                <p><strong>Patrimônio: </strong> 271</p>
+                <p><strong>Série: </strong> 2P3MOT3</p>
+                <p><strong>Modelo: </strong> Dell Vostro 15 3510</p>
+            </div>
+            <div class="device">
+                <h4>🎧 Fone</h4>
+                <p><strong>Patrimônio: </strong> 835</p>
+                <p><strong>Série: </strong> 2309ME017VY8</p>
+                <p><strong>Modelo: </strong> Logitech H390</p>
+            </div>
+            <div class="device">
+                <h4>📱 Celular</h4>
+                <p><strong>Patrimônio: </strong> 846</p>
+                <p><strong>Série: </strong> UJ5MVC3MT8</p>
+                <p><strong>Modelo: </strong> Motorola E13</p>
+                <p><strong>IMEI 1: </strong> 35934098125427</p>
+                <p><strong>IMEI 2: </strong> 359340981254284</p>
+            </div>
+            <div class="device">
+                <h4>🔩 Suporte</h4>
+                <p><strong>Patrimônio: </strong> 1245</p>
+                <p><strong>Série: </strong> (Não fornecido)</p>
+                <p><strong>Modelo: </strong> Alumínio</p>
+            </div>
+        </div>
+        <div class="devices">
+            <div class="responsibility-link">
+                <h4><a href="">📑 Termo de Responsabilidade</a></h4>
+            </div>
+        </div>
+    </div>
+</div>
+<footer>
+    <p>&copy; 2024 - 2025 Inventário de Colaboradores - Todos os direitos reservados.</p>
+</footer>
+
+</body>
+</html>
