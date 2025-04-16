6dea6f9c1ea3d01b131bf846a7afb584927cc5c0
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2025-04-16 13:58:10 -0300

    ✨ feat: adiciona nova seção para colaboradores com inicial W
    Adiciona uma nova página HTML para listar colaboradores cujos nomes começam com a letra W. A página inclui informações detalhadas sobre cada colaborador, como cargo, departamento, e-mail, CPF, centro de custo, e equipamentos associados. Esta adição visa melhorar a organização e acessibilidade das informações dos colaboradores, facilitando a consulta e o gerenciamento de dados.

---
+++ colaboradores/Secao W.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Seção W</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Marcelo de Araujo Almeida">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="icon" href="../favicon/favicon-16x16.png">
+    <link rel="stylesheet" href="../css/colaboradores.css">
+    <link rel="stylesheet" href="../css/menu-drop.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="../scripts/menudrop.js" defer></script>
+</head>
+
+<body>
+<?php include '../includes/header.php'; ?>
+<h2 style="text-align: center;margin-top: 10px">📋 Colaboradores Letras W</h2>
+<div class="container">
+    <div class="card">
+        <img src="../imagem/Walkiria%20Silva.png" alt="Foto do Colaborador" class="profile-pic">
+        <h3>👤 Walkiria Oliveira Silva</h3>
+        <p><strong>Cargo: </strong> Analista Adminsitrativo PL</p>
+        <p><strong>Departamento: </strong> Operações Plano</p>
+        <p><strong>E-mail: </strong> walkiria.silva@amorsaude.com</p>
+        <p><strong>CPF: </strong> 067.368.296-07</p>
+        <p><strong>Centro de Custo: </strong> 72024 - Operações Plano</p>
+        <p><strong>: </strong> online</p>
+        <div class="devices">
+            <div class="device">
+                <h4>🎧 Fone</h4>
+                <p><strong>Patrimônio: </strong> 921</p>
+                <p><strong>Série: </strong> 2316ME02F8H8</p>
+                <p><strong>Modelo: </strong> Logitech H390</p>
+            </div>
+            <div class="device">
+                <h4>⌨️🖱️ Teclado e Mouse</h4>
+                <p><strong>Patrimônio: </strong> 490</p>
+                <p><strong>Série: </strong> 2308LZX49CD8 - 2310CE1A2BB8</p>
+                <p><strong>Modelo: </strong> Logitech M185 - K270</p>
+            </div>
+            <div class="device">
+                <h4>📱 Celular</h4>
+                <p><strong>Patrimônio: </strong> 810</p>
+                <p><strong>Série: </strong> R9XW20CHL4K</p>
+                <p><strong>Modelo: </strong> Galaxy A03 Core</p>
+                <p><strong>IMEI 1: </strong> 358302235393608</p>
+                <p><strong>IMEI 2: </strong> 359796405393604</p>
+            </div>
+            <div class="device">
+                <h4>💻 Notebook</h4>
+                <p><strong>Patrimônio: </strong> 489</p>
+                <p><strong>Série: </strong> HS35KZ3</p>
+                <p><strong>Modelo: </strong> Dell Vostro 3520</p>
+            </div>
+        </div>
+        <h2>Equipamentos Comprometidos</h2>
+        <div class="devices">
+            <div class="device">
+                <h4>📱 Celular</h4>
+                <p><strong>Patrimônio: </strong> 794</p>
+                <p><strong>Série: </strong> RX9R90HDAAR</p>
+                <p><strong>Modelo: </strong> Galaxy A02</p>
+                <p><strong>IMEI 1: </strong> 354397310709045</p>
+                <p><strong>IMEI 2: </strong> 354578130709048</p>
+            </div>
+        </div>
+
+        <div class="devices">
+            <div class="responsibility-link">
+                <h4><a href="">📑 Termo de Responsabilidade</a></h4>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <img src="../imagem/Wendell%20Oliveira.png" alt="Foto do Colaborador" class="profile-pic">
+        <h3>👤 Wendell Alessandro Candido Oliveira</h3>
+        <p><strong>Cargo: </strong> Analista de Pessoas & Cultura Sênior</p>
+        <p><strong>Departamento: </strong> Pessoas & Cultura</p>
+        <p><strong>E-mail: </strong> wendell.oliveira@amorsaude.com</p>
+        <p><strong>CPF: </strong> 411.412.878-62</p>
+        <p><strong>Centro de Custo: </strong> 13006 - Pessoas & Cultura</p>
+        <div class="devices">
+            <div class="device">
+                <h4>💻 Notebook</h4>
+                <p><strong>Patrimônio: </strong> 1446</p>
+                <p><strong>Série: </strong> B832JZ3</p>
+                <p><strong>Modelo: </strong> Dell Vostro 3520</p>
+            </div>
+            <div class="device">
+                <h4>🎧 Fone</h4>
+                <p><strong>Patrimônio: </strong> 966</p>
+                <p><strong>Série: </strong> 2329AYO1A558</p>
+                <p><strong>Modelo: </strong> Logitech H390</p>
+            </div>
+            <div class="device">
+                <h4>⌨️🖱️ Teclado e Mouse</h4>
+                <p><strong>Patrimônio: </strong> 1445</p>
+                <p><strong>Série: </strong> 2232CE1CFB58</p>
+                <p><strong>Modelo: </strong> MK270</p>
+            </div>
+            <div class="device">
+                <h4>🔩 Suporte</h4>
+                <p><strong>Patrimônio: </strong> 967</p>
+                <p><strong>Modelo: </strong> Alumínio</p>
+            </div>
+            <div class="device">
+                <h4>🖥️ Monitor</h4>
+                <p><strong>Patrimônio: </strong> 1517</p>
+                <p><strong>Série: </strong> 312AZVN60015</p>
+                <p><strong>Modelo: </strong> LG 22MP410-B</p>
+            </div>
+            <div class="device">
+                <h4>📱 Celular</h4>
+                <p><strong>Patrimônio: </strong> 968</p>
+                <p><strong>Série: </strong> ZF5248W28G</p>
+                <p><strong>IMEI 1: </strong> 359340983540177/05</p>
+                <p><strong>IMEI 2: </strong> 359340983540185/05</p>
+            </div>
+        </div>
+        <div class="devices">
+            <div class="responsibility-link">
+                <h4><a href="">📑 Termo de Responsabilidade</a></h4>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <img src="../imagem/William%20Paulino%20Lima.png" alt="Foto do Colaborador" class="profile-pic">
+        <h3>👤 William Paulino Lima</h3>
+        <p><strong>Cargo: </strong> Gerente de Canais Digitais e Relacionamento</p>
+        <p><strong>Departamento: </strong> Dir. Operações</p>
+        <p><strong>E-mail: </strong> william.lima@amorsaude.com</p>
+        <p><strong>CPF: </strong> 336.273.218-63</p>
+        <p><strong>Centro de Custo: </strong> 22000 - Dir. Operações</p>
+        <p><strong>: </strong> Desligado</p>
+        <p><strong>Data de Desligamento: </strong> 08.01.2025</p>
+        <div class="devices">
+            <div class="device">
+                <h4>📱 Celular</h4>
+                <p><strong>Patrimônio: </strong> 994</p>
+                <p><strong>Série: </strong> zl73239kd6</p>
+                <p><strong>Modelo: </strong> Moto E13</p>
+            </div>
+        </div>
+        <h2>Equipamentos Comprometidos</h2>
+        <div class="device">
+            <h4>📱 Smartphone</h4>
+            <p><strong>Patrimônio: </strong> 747</p>
+            <p><strong>Série: </strong> R9xt20139dl</p>
+            <p><strong>Modelo: </strong> Samsung Galaxy A03</p>
+            <p><strong>IMEI 1: </strong> 354985980029619/01</p>
+            <p><strong>IMEI 2: </strong> 355106270029610/01</p>
+            <p><strong>ID Ticket: </strong> #30250</p>
+        </div>
+        <div class="attention-info">
+            <h2>⚠️ Informações Importantes</h2>
+            <p><strong>Alerta: </strong> O colaborador está utilizando os equipamentos, porém somente possuímos o
+                termo de responsabilidade assinado para o celular e sem dados dos demais.</p>
+        </div>
+        <div class="devices">
+            <div class="responsibility-link">
+                <h4><a href="">📑 Termo de Responsabilidade</a></h4>
+            </div>
+        </div>
+    </div>
+</div>
+<footer>
+    <p>&copy; 2024 - 2025 Inventário de Colaboradores - Todos os direitos reservados.</p>
+</footer>
+
+</body>
+</html>
