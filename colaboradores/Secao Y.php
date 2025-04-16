aaba0e5b14d22da7a66d6459771fdf3d2dfac15c
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2025-04-16 13:58:22 -0300

    ✨ feat: adiciona nova seção Y para colaboradores
    A nova página "Seção Y" foi criada para listar colaboradores cujos nomes começam com a letra Y. Esta adição melhora a organização e acessibilidade das informações dos colaboradores, permitindo uma navegação mais intuitiva e segmentada. A página inclui detalhes pessoais e profissionais, além de informações sobre equipamentos alocados a cada colaborador.

---
+++ colaboradores/Secao Y.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Seção Y</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Marcelo de Araujo Almeida">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="icon" href="../favicon/favicon-16x16.png">
+    <link rel="stylesheet" href="../css/colaboradores.css">
+    <link rel="stylesheet" href="../css/menu-drop.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="../scripts/menudrop.js" defer></script>
+</head>
+
+<body>
+<?php include '../includes/header.php'; ?>
+<h2 style="text-align: center;margin-top: 10px">📋 Colaboradores Letras Y</h2>
+<div class="container">
+    <div class="card">
+        <img src="../imagem/Yago%20Mariano.png" alt="Foto do Colaborador" class="profile-pic">
+        <h3>👤 Yago dos Anjos Mariano</h3>
+        <p><strong>Cargo: </strong> Consultor de Operações</p>
+        <p><strong>Departamento: </strong> Consultoria</p>
+        <p><strong>E-mail: </strong> yago.mariano@amorsaude.com</p>
+        <p><strong>CPF: </strong> 120.120.005-42</p>
+        <p><strong>Centro de Custo: </strong> 12006 - Regional 1</p>
+        <p><strong>: </strong> online</p>
+        <p><strong>Data de Admissão: </strong> 03.06.2023</p>
+        <div class="devices">
+            <div class="device">
+                <h4>🎧 Fone</h4>
+                <p><strong>Patrimônio: </strong> 1008</p>
+                <p><strong>Série: </strong> 2350AYO52C48</p>
+                <p><strong>Modelo: </strong> Logitech H390</p>
+            </div>
+            <div class="device">
+                <h4>⌨️🖱️ Teclado e Mouse</h4>
+                <p><strong>Patrimônio: </strong> 1520</p>
+                <p><strong>Série: </strong> 2334CE2DA858</p>
+                <p><strong>Modelo: </strong> Logitech MK270</p>
+            </div>
+            <div class="device">
+                <h4>🔩 Suporte</h4>
+                <p><strong>Patrimônio: </strong> 1219</p>
+                <p><strong>Série: </strong> Não disponível</p>
+                <p><strong>Modelo: </strong> Alumínio</p>
+            </div>
+            <div class="device">
+                <h4>🔌 Carregador</h4>
+                <p><strong>Patrimônio: </strong> 1222</p>
+                <p><strong>Série: </strong> SA18D53739</p>
+                <p><strong>Modelo: </strong> MC-107</p>
+            </div>
+            <div class="device">
+                <h4>🔌 Fonte</h4>
+                <p><strong>Patrimônio: </strong> 1220</p>
+                <p><strong>Série: </strong> SA11M40865</p>
+                <p><strong>Modelo: </strong> PA-1650-74</p>
+            </div>
+            <div class="device">
+                <h4>💻 Notebook</h4>
+                <p><strong>Patrimônio: </strong> 1521</p>
+                <p><strong>Série: </strong> PE9014206100</p>
+                <p><strong>Modelo: </strong> Lenovo V15 G3</p>
+            </div>
+            <div class="device">
+                <h4>📱 Smartphone</h4>
+                <p><strong>Patrimônio: </strong> 1221</p>
+                <p><strong>Série: </strong> SVNBD74575</p>
+                <p><strong>Modelo: </strong> Moto E22</p>
+                <p><strong>IMEI 1: </strong> 353689479970934</p>
+                <p><strong>IMEI 2: </strong> 353689479970942</p>
+            </div>
+        </div>
+        <div class="devices">
+            <div class="responsibility-link">
+                <h4><a href="">📑 Termo de Responsabilidade</a></h4>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <img src="../imagem/Yago%20Oliveira.png" alt="Foto do Colaborador" class="profile-pic">
+        <h3>👤 Yago Romao Rocha Oliveira</h3>
+        <p><strong>Cargo: </strong> Analista CS SAC Jr</p>
+        <p><strong>Departamento: </strong> SAC</p>
+        <p><strong>E-mail: </strong> yago.oliveira@amorsaude.com</p>
+        <p><strong>CPF: </strong> 380.127.808-52</p>
+        <p><strong>Centro de Custo: </strong> 12002 - SAC</p>
+        <p><strong>: </strong> online</p>
+        <div class="devices">
+            <div class="device">
+                <h4>⌨️Teclado</h4>
+                <p><strong>Patrimônio (Teclado): </strong> 347</p>
+                <p><strong>Série (Teclado): </strong> 2234CE182209</p>
+                <p><strong>Patrimônio (Mouse): </strong> 348</p>
+            </div>
+            <div class="device">
+                <h4>🖱️ Mouse</h4>
+                <p><strong>Série (Mouse): </strong> 2230LZN0ZN78</p>
+                <p><strong>Modelo: </strong> Logitech MK270</p>
+            </div>
+            <div class="device">
+                <h4>🔩 Suporte</h4>
+                <p><strong>Patrimônio: </strong> 332</p>
+                <p><strong>Série: </strong> Não disponível</p>
+                <p><strong>Modelo: </strong> Alumínio</p>
+            </div>
+            <div class="device">
+                <h4>💻 Notebook</h4>
+                <p><strong>Patrimônio: </strong> 195</p>
+                <p><strong>Série: </strong> PE0975GF</p>
+                <p><strong>Modelo: </strong> Lenovo V15 82ME</p>
+            </div>
+            <div class="device">
+                <h4>🎧 Fone</h4>
+                <p><strong>Patrimônio: </strong> 621</p>
+                <p><strong>Série: </strong> 273962288</p>
+                <p><strong>Modelo: </strong> Jabra HSC016</p>
+            </div>
+            <div class="device">
+                <h4>📱 Celular</h4>
+                <p><strong>Patrimônio: </strong> 622</p>
+                <p><strong>Série: </strong> R9XT806p4sk</p>
+                <p><strong>Modelo: </strong> Galaxy A03</p>
+                <p><strong>IMEI 1: </strong> 350916873113230/01</p>
+                <p><strong>IMEI 2: </strong> 354654233113238/01</p>
+            </div>
+        </div>
+        <div class="devices">
+            <div class="responsibility-link">
+                <h4><a href="">📑 Termo de Responsabilidade</a></h4>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <img src="../imagem/Yasmin%20Silva.png" alt="Foto do Colaborador" class="profile-pic">
+        <h3>👤 Yasmin Campos da Silva</h3>
+        <p><strong>Cargo: </strong> Assistente de Operações</p>
+        <p><strong>Departamento: </strong> Backoffice</p>
+        <p><strong>E-mail: </strong> Não informado</p>
+        <p><strong>CPF: </strong> Não informado</p>
+        <p><strong>Centro de Custo: </strong> 12005 - Backoffice</p>
+        <p><strong>: </strong> Desligada (19.08.2024)</p>
+        <div class="devices">
+            <div class="device">
+                <h4>💻 Notebook</h4>
+                <p><strong>Patrimônio: </strong> 105</p>
+                <p><strong>Série: </strong> PE08znfj</p>
+                <p><strong>Modelo: </strong> Lenovo IdeaPad 3 15ALC6</p>
+            </div>
+            <div class="device">
+                <h4>🎧 Fone</h4>
+                <p><strong>Patrimônio: </strong> 906</p>
+                <p><strong>Série: </strong> 23298GG15AB8</p>
+                <p><strong>Modelo: </strong> Logitech H390</p>
+            </div>
+            <div class="device">
+                <h4>⌨️🖱️ Teclado e Mouse</h4>
+                <p><strong>Patrimônio: </strong> 1456</p>
+                <p><strong>Série: </strong> 2232CE1C72B8</p>
+                <p><strong>Modelo: </strong> Logitech MK270</p>
+            </div>
+            <div class="device">
+                <h4>🔩 Suporte</h4>
+                <p><strong>Patrimônio: </strong> 1151</p>
+                <p><strong>Série: </strong> Não disponível</p>
+                <p><strong>Modelo: </strong> Alumínio</p>
+            </div>
+            <div class="device">
+                <h4>🖥️ Monitor</h4>
+                <p><strong>Patrimônio: </strong> 002</p>
+                <p><strong>Série: </strong> BR-00CPG1-TVB00-9CA-11Eu-A08</p>
+                <p><strong>Modelo: </strong> Dell P2018H</p>
+            </div>
+            <div class="device">
+                <h4>📱 Celular</h4>
+                <p><strong>Patrimônio: </strong> 510</p>
+                <p><strong>Série: </strong> r9qt402fxwy</p>
+                <p><strong>Modelo: </strong> Samsung A03</p>
+                <p><strong>IMEI 1: </strong> 351989271794777/01</p>
+                <p><strong>IMEI 2: </strong> 353297231794772/01</p>
+            </div>
+        </div>
+        <h2>Equipamentos Danificados</h2>
+        <div class="device">
+            <h4>🎧 Fone</h4>
+            <p><strong>Patrimônio: </strong> 530</p>
+            <p><strong>Série: </strong> 230307953</p>
+            <p><strong>Modelo: </strong> Jabra HSC016</p>
+        </div>
+
+        <div class="devices">
+            <div class="responsibility-link">
+                <h4><a href="">📑 Termo de Responsabilidade</a></h4>
+            </div>
+        </div>
+    </div>
+</div>
+<footer>
+    <p>&copy; 2024 - 2025 Inventário de Colaboradores - Todos os direitos reservados.</p>
+</footer>
+
+</body>
+</html>
