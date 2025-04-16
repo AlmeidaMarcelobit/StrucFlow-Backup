eab2664e836299d372103215f1fd8738971daf98
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2025-04-16 13:55:00 -0300

    ✨ feat: adiciona nova página para seção de colaboradores "O"
    Adiciona uma nova página HTML para a seção de colaboradores "O",
    incluindo informações detalhadas sobre o colaborador Otavio Honrorio
    de Freitas. A página contém dados pessoais, informações de contato,
    e detalhes sobre os dispositivos alocados ao colaborador. Esta
    adição visa melhorar a organização e acessibilidade das informações
    dos colaboradores, facilitando a consulta e gestão de recursos.

---
+++ colaboradores/Secao O.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Seção O</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Marcelo de Araujo Almeida">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="icon" href="../favicon/favicon-16x16.png">
+    <link rel="stylesheet" href="../css/colaboradores.css">
+    <link rel="stylesheet" href="../css/menu-drop.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="../scripts/menudrop.js" defer></script>
+</head>
+
+<body>
+<?php include '../includes/header.php'; ?>
+<h2 style="text-align: center;margin-top: 10px">📋 Colaboradores Letras 0</h2>
+<div class="container">
+    <div class="card">
+        <img src="../imagem/Sem%20Foto.png" alt="Foto do Colaborador" class="profile-pic">
+        <h3>👤 Otavio Honrorio de Freitas</h3>
+        <p><strong>Cargo: </strong>Product Owner Junior</p>
+        <p><strong>Departamento: </strong>Projetos e Processos</p>
+        <p><strong>E-mail: </strong>otavio.freitas@amorsaude.com</p>
+        <p><strong>CPF: </strong>431.693.188-10</p>
+        <p><strong>Centro de Custo: </strong>13004 - Projetos / Processos</p>
+        <p><span class=" online">:  online</span></p>
+        <div class="devices">
+            <div class="device">
+                <h4>💻 Notebook</h4>
+                <p><strong>Patrimônio: </strong>190</p>
+                <p><strong>Série: </strong>pe09ftjn</p>
+                <p><strong>Modelo: </strong>Lenovo Ideapad3 15alc6</p>
+            </div>
+            <div class="device">
+                <h4>⌨️🖱️Teclado e Mouse</h4>
+                <p><strong>Patrimônio: </strong>237</p>
+                <p><strong>Série: </strong>2253CE0F1789 - 2248LZX6ND08</p>
+                <p><strong>Modelo: </strong>Logitech MK270</p>
+            </div>
+            <div class="device">
+                <h4>🎧 Fone</h4>
+                <p><strong>Patrimônio: </strong>601</p>
+                <p><strong>Série: </strong>265441361</p>
+                <p><strong>Modelo: </strong>Jabra HSC016</p>
+            </div>
+            <div class="device">
+                <h4>🔩 Suporte</h4>
+                <p><strong>Patrimônio: </strong>803</p>
+                <p><strong>Série: </strong></p>
+                <p><strong>Modelo: </strong>Alumínio</p>
+            </div>
+            <div class="device">
+                <h4>🖥️ Monitor</h4>
+                <p><strong>Patrimônio: </strong>397</p>
+                <p><strong>Série: </strong>303AZPU94384</p>
+                <p><strong>Modelo: </strong>LG 22MP410-B</p>
+            </div>
+        </div>
+        <div class="devices">
+            <div class="responsibility-link">
+                <h4><a href="">📑 Termo de Responsabilidade</a></h4>
+            </div>
+        </div>
+    </div>
+</div>
+<footer>
+    <p>&copy; 2024 - 2025 Inventário de Colaboradores - Todos os direitos reservados.</p>
+</footer>
+
+</body>
+</html><?php
