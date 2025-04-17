14a07d00b42ae1490276f55331b757ec60e9159c
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2025-04-17 10:04:17 -0300

    ✨ feat: adiciona novas seções ao menu de colaboradores
    Novas seções de D a Y foram adicionadas ao menu de colaboradores
    no arquivo de cabeçalho. Isso expande a navegação, permitindo
    acesso direto a mais áreas de conteúdo, melhorando a usabilidade
    e organização do site.

--- includes/header.php
+++ includes/header.php
@@ -9,6 +9,25 @@
                     <li><a href="../colaboradores/Secao%20A.php">Seção A</a></li>
                     <li><a href="../colaboradores/Secao%20B.php">Seção B</a></li>
                     <li><a href="../colaboradores/Secao%20C.php">Seção C</a></li>
+                    <li><a href="../colaboradores/Secao%20D.php">Seção D</a></li>
+                    <li><a href="../colaboradores/Secao%20E.php">Seção E</a></li>
+                    <li><a href="../colaboradores/Secao%20F.php">Seção F</a></li>
+                    <li><a href="../colaboradores/Secao%20G.php">Seção G</a></li>
+                    <li><a href="../colaboradores/Secao%20H.php">Seção H</a></li>
+                    <li><a href="../colaboradores/Secao%20I.php">Seção I</a></li>
+                    <li><a href="../colaboradores/Secao%20J.php">Seção J</a></li>
+                    <li><a href="../colaboradores/Secao%20K.php">Seção K</a></li>
+                    <li><a href="../colaboradores/Secao%20L.php">Seção L</a></li>
+                    <li><a href="../colaboradores/Secao%20M.php">Seção M</a></li>
+                    <li><a href="../colaboradores/Secao%20N.php">Seção N</a></li>
+                    <li><a href="../colaboradores/Secao%20O.php">Seção O</a></li>
+                    <li><a href="../colaboradores/Secao%20P.php">Seção P</a></li>
+                    <li><a href="../colaboradores/Secao%20R.php">Seção R</a></li>
+                    <li><a href="../colaboradores/Secao%20S.php">Seção S</a></li>
+                    <li><a href="../colaboradores/Secao%20T.php">Seção T</a></li>
+                    <li><a href="../colaboradores/Secao%20V.php">Seção V</a></li>
+                    <li><a href="../colaboradores/Secao%20W.php">Seção W</a></li>
+                    <li><a href="../colaboradores/Secao%20Y.php">Seção Y</a></li>
                 </ul>
             </li>
 
