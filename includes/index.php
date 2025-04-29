e495dfc6c9f49ed2f9f5bd18cc304f008fec3049
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2025-04-29 15:16:26 -0300

    ♻️ (index.php): refatora a inclusão de cabeçalho e rodapé para arquivos separados
    A inclusão de metadados e scripts no cabeçalho foi movida para um arquivo separado, `head.html`, para melhorar a organização e a reutilização do código. Isso facilita a manutenção e a atualização do cabeçalho em várias páginas. Além disso, o rodapé foi extraído para `footer.php`, permitindo uma gestão centralizada do conteúdo do rodapé. A classe do contêiner principal foi renomeada de `container` para `home` para refletir melhor o propósito da página inicial.

--- includes/index.php
+++ includes/index.php
@@ -3,15 +3,9 @@
 
 <head>
     <title>Home</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="author" content="Marcelo de Araujo Almeida">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
-    <link rel="icon" type="image/png" sizes="32x32" href="../favicon/favicon-16x16.png">
-    <link rel="stylesheet" href="../css/index.css">
-    <link rel="stylesheet" href="../css/menu-drop.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="../scripts/menudrop.js" defer></script>
+    <?php
+    include '../includes/head.html';
+    ?>
 </head>
 
 <body>
@@ -21,15 +15,15 @@ include '../includes/header.php';
 ?>
 
 <!-- Conteúdo Principal -->
-<div class="container">
+<div class="home">
     <h1>Bem-vindo ao sistema de inventário!</h1>
     <p>InventType é uma plataforma eficiente para a gestão de colaboradores e controle de equipamentos. Com ele, você pode visualizar informações detalhadas sobre os funcionários, incluindo nome, CPF e status na empresa (ativo ou desligado). Além disso, o sistema permite gerenciar o inventário de equipamentos, identificando itens em estoque, em bom estado ou danificados. Com uma interface intuitiva, o SaúdeTracker facilita a organização e otimização dos recursos tecnológicos da sua empresa.</p>
 </div>
 
 <!-- Footer -->
-<footer>
-    <p>&copy; 2024 - 2025 SaúdeTracker - Todos os direitos reservados.</p>
-</footer>
+<?php
+include '../includes/footer.php';
+?>
 </body>
 
 </html>
