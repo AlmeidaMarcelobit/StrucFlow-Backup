312fcb3fafbdf80a05705d59090cdb6d175b33b4
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2025-04-29 15:15:59 -0300

    📝 (footer.php): atualiza o nome da entidade no rodapé
    O nome "Inventário de Colaboradores" é alterado para "InventType" no rodapé.
    Essa mudança reflete uma atualização na marca ou nome da entidade, garantindo
    que a documentação e a interface do usuário estejam alinhadas com a identidade
    atual da organização.

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,3 @@
     <footer>
-        <p>&copy; 2024 - 2025 Inventário de Colaboradores - Todos os direitos reservados.</p>
+        <p>&copy; 2024 - 2025 InventType - Todos os direitos reservados.</p>
     </footer>
