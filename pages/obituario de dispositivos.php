d9ee8d5c820c60f01f0f51472e5d7a5f53e73efa
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2025-04-30 17:26:56 -0300

    ✨13.8.0 - (obituario de dispositivos.php): adiciona cabeçalhos de categorias de dispositivos
    Adiciona cabeçalhos para diferentes categorias de dispositivos na página "obituário de dispositivos". Isso melhora a organização visual e a clareza da página, permitindo que os usuários identifiquem rapidamente as seções dedicadas a cada tipo de dispositivo, como Celulares, Fone, Teclado e Mouse, Notebooks e Monitores.

--- pages/obituario de dispositivos.php
+++ pages/obituario de dispositivos.php
@@ -7,7 +7,11 @@
 <body>
 <?php include '../includes/header.php'; ?>
 <div class="container">
-
+    <h1>Celulares</h1>
+    <h1>Fone</h1>
+    <h1>Teclado e Mouse</h1>
+    <h1>Notebooks</h1>
+    <h1>Monitores</h1>
 </div>
 <?php include '../includes/footer.php'; ?>
 </body>
