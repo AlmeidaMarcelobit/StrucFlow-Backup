a8e43d9c97bc87833539c19c056aacb5439d0402
Marcelo Almieda <124610030+AlmeidaMarcelobit@users.noreply.github.com>
2025-04-30 15:15:46 -0300

    ✨13.5.0 - (obituario de dispositivos.php): adiciona nova página "Obituário de Dispositivos"
    Cria uma nova página em PHP para o "Obituário de Dispositivos".
    Esta página serve como um espaço dedicado para listar ou homenagear
    dispositivos que não estão mais em uso. A estrutura básica do HTML
    é configurada, incluindo cabeçalho e rodapé padrão, para garantir
    consistência com o restante do site.

---
+++ pages/obituario de dispositivos.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Obituário de Dispositivos</title>
+    <?php include '../includes/head.html'; ?>
+</head>
+<body>
+<?php include '../includes/header.php'; ?>
+<div class="container">
+
+</div>
+<?php include '../includes/footer.php'; ?>
+</body>
+</html>
